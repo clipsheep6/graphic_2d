f9dd3992ebac05aabf622de3305525276133b4a0
luwh0708 <luwh0708@thundersoft.com>
2021-09-07 10:20:31 +0800

    fix subwindow video sptr
    Change-Id: I32327d7260b45025692c82f2b03fbdeaa554c05d

--- frameworks/wm/include/subwindow_video_impl.h
+++ frameworks/wm/include/subwindow_video_impl.h
@@ -58,7 +58,7 @@ private:
 
     // base attribute
     WindowAttribute attr;
-    sptr<Window> parentWindow = nullptr;
+    wptr<Window> pw = nullptr;
     uint32_t layerId = 0;
 
     // functional member
--- frameworks/wm/include/window_impl.h
+++ frameworks/wm/include/window_impl.h
@@ -98,8 +98,6 @@ public:
     virtual WMError OnTouchCancel(TouchCancelFunc func) override;
     virtual WMError OnTouchShape(TouchShapeFunc func) override;
     virtual WMError OnTouchOrientation(TouchOrientationFunc func) override;
-    int32_t GetPosX() const;
-    int32_t GetPosY() const;
 
 private:
     WindowImpl() = default;

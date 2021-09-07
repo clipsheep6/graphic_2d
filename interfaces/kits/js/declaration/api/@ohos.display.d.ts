7321e6d8adb366c8e7c4f3b87afd4edc4197a951
xiaotong.huo <xiaotong.huo@thundersoft.com>
2021-09-07 17:11:05 +0800

    impl display napi, refactor window napi
    Change-Id: Ibc6ffd8bb6825053d5aa59ee6c1d208140f5ed85

---
+++ interfaces/kits/js/declaration/api/@ohos.display.d.ts
@@ -0,0 +1,120 @@
+/**
+ * interface of display manager
+ * @devices tv, phone, tablet, wearable
+ */
+declare namespace display {
+  /**
+   * get the default display
+   * @devices tv, phone, tablet, wearable
+   */
+  function getDefaultDisplay(): Promise<Display>;
+
+  /**
+  /**
+   * the state of display
+   * @devices tv, phone, tablet, wearable
+   */
+  enum DisplayState {
+    /**
+     * unknown
+     */
+    STATE_UNKNOWN = 0,
+    /**
+     * screen off
+     */
+    STATE_OFF,
+    /**
+     * screen on
+     */
+    STATE_ON,
+    /**
+     * doze, but it will update for some important system messages
+     */
+    STATE_DOZE,
+    /**
+     * doze and not update
+     */
+    STATE_DOZE_SUSPEND,
+    /**
+     * VR node
+     */
+    STATE_VR,
+    /**
+     * screen on and not update
+     */
+    STATE_ON_SUSPEND,
+  }
+
+  /**
+   * Properties of display, it couldn't update automatically
+   * @devices tv, phone, tablet, wearable
+   */
+  interface Display {
+    /**
+     * display id
+     */
+    id: number;
+
+    /**
+     * display name
+     */
+    name: string;
+
+    /**
+     * the display is alive
+     */
+    alive: boolean;
+
+    /**
+     * the state of display
+     */
+    state: DisplayState;
+
+    /**
+     * refresh rate, unit: Hz
+     */
+    refreshRate: number;
+
+    /**
+     * the rotation degrees of the display
+     */
+    rotation: number;
+
+    /**
+     * the width of display, unit: pixel
+     */
+    width: number;    
+    
+    /**
+     * the height of display, unit: pixel
+     */
+    height: number;
+
+    /**
+     * indicates the display resolution.
+     */
+    densityDPI: number;
+
+    /**
+     * indicates the display density in pixels. The value of a low-resolution display is 1.0
+     */
+    densityPixels: number;
+
+    /**
+     * indicates the text scale density of a display.
+     */
+    scaledDensity: number;
+
+    /**
+     * the DPI on X-axis.
+     */
+    xDPI: number;
+
+    /**
+     * the DPI on Y-axis.
+     */
+    yDPI: number;
+  }
+}
+
+export default display;

4bf4a8f636febecf5d97f7881e673d868cafa3d5
bayanxing <bayanxing@kaihongdigi.com>
2022-01-19 10:21:17 +0800

    solve code check

--- frameworks/bootanimation/data/generate_tool/raw_maker.py
+++ frameworks/bootanimation/data/generate_tool/raw_maker.py
@@ -156,7 +156,7 @@ class RawMaker:
                         break
                 screen_old_bytes = screen_now_bytes
             proc = int(num / frame_count * 30)
-            print("|" , ("=" * proc), end="")
+            print("|", ("=" * proc), end="")
             print("%s|" % (" " * (30 - proc)), end="")
             print("%.2f%%" % (num / frame_count * 100), end="", flush=True)
             if start_pos == 3 or end_pos == 6:

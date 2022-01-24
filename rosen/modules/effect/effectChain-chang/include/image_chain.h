254cc5aecae5502ee09b44e7850855a9c1a888ff
xiongwei <xiongwei84@huawei.com>
2022-01-24 12:56:03 +0800

    Static check 3
    Signed-off-by: xiongwei <xiongwei84@huawei.com>
    Change-Id: I25ca69fb87a46a90ec35718cfd2294fa00bd1b90

---
+++ rosen/modules/effect/effectChain-chang/include/image_chain.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IMAGE_CHAIN_H
+#define IMAGE_CHAIN_H
+
+#include "algo_filter.h"
+#include "input.h"
+
+class ImageChain {
+public:
+    ImageChain(std::vector< Input* > inputs);
+    void Render();
+private:
+    void CreatTexture(unsigned int& TextureID);
+    void SeriesRendering();
+    void ParallelRendering();
+    bool _flagSeries = false;
+    unsigned int _frameBuffer;
+    unsigned int _srcTexture;
+    unsigned int _outputTexture;
+    unsigned int _resultTexture;
+    std::vector< Input* > myInputs_;
+};
+
+#endif
---
+++ rosen/modules/effect/effectChain-chang/src/program.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "program.h"
+
+using namespace std;
+
+void Program::Compile(string vertexShader, string fragmentShader) {
+    vertexID_ = CreateShader(GL_VERTEX_SHADER, vertexShader);
+    fragmentID_ = CreateShader(GL_FRAGMENT_SHADER, fragmentShader);
+    if (!initFlag_) {
+        initFlag_ = true;
+        programID_ = glCreateProgram();
+    }
+    glAttachShader(programID_, vertexID_);
+    glAttachShader(programID_, fragmentID_);
+    glLinkProgram(programID_);
+    CheckProgramCompileErrors(programID_);
+    glDeleteShader(vertexID_);
+    glDeleteShader(fragmentID_);
+}
+
+void Program::UseProgram() const {
+    glUseProgram(programID_);
+}
+
+GLuint Program::CreateShader(GLuint type, const string& shaderCode) {
+    const GLchar* charShaderCode = (const GLchar*)shaderCode.c_str();
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &charShaderCode, nullptr);
+    glCompileShader(shader);
+    CheckShaderCompileErrors(shader);
+    return shader;
+}
+
+void Program::CheckShaderCompileErrors(GLuint shader) {
+    GLint complete = 0;
+    GLchar infoLog[1024];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &complete);
+    if (!complete) {
+        glGetShaderInfoLog(shader, 1024, nullptr, infoLog);;
+    }
+}
+
+void Program::CheckProgramCompileErrors(GLuint program) {
+    GLint complete = 0;
+    GLchar infoLog[1024];
+    glGetProgramiv(program, GL_COMPILE_STATUS, &complete);
+    if (!complete) {
+        glGetProgramInfoLog(program, 1024, nullptr, infoLog);
+    }
+}

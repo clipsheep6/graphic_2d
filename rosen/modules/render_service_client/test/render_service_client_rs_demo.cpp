56c2f1f0dc7d9b4eab0432c7e950bc9f118104e2
hwx1027414 <huangzhilin4@huawei.com>
2022-01-24 17:01:30 +0800

    graphic standard rosen UT update
    Signed-off-by: hwx1027414 <huangzhilin4@huawei.com>
    Change-Id: If3688bf83dece1e3cda44adb41ba4e51726b5be3

--- rosen/modules/render_service_client/test/render_service_client_rs_demo.cpp
+++ rosen/modules/render_service_client/test/render_service_client_rs_demo.cpp
@@ -28,9 +28,9 @@
 #include "ui/rs_canvas_node.h"
 #include "ui/rs_surface_extractor.h"
 #include "ui/rs_ui_director.h"
-#include "core/transaction/rs_interfaces.h"
-#include "core/ui/rs_display_node.h"
-#include "core/ui/rs_surface_node.h"
+#include "transaction/rs_interfaces.h"
+#include "ui/rs_display_node.h"
+#include "ui/rs_surface_node.h"
 #include "render_context/render_context.h"
 // temporary debug
 #include "foundation/graphic/standard/rosen/modules/render_service_base/src/platform/ohos/rs_surface_frame_ohos.h"
@@ -38,12 +38,26 @@
 
 using namespace OHOS;
 using namespace OHOS::Rosen;
-using namespace std;
 
 std::unique_ptr<RSSurfaceFrame> framePtr;
+#ifdef ACE_ENABLE_GPU
 RenderContext* rc_ = nullptr;
+#endif
+namespace
+{
+    constexpr bool wrongExit = false;
+    constexpr bool successExit = false;
+    struct Circle
+    {
+        Circle(float x, float y, float r)
+         :x_(x), y_(y), r_(r) {};
+        float x_ = 0.0f;
+        float y_ = 0.0f;
+        float r_ = 0.0f;
+    }; //
+} // anonymous namespace
 
-void DrawSurface(
+bool DrawSurface(
     SkRect surfaceGeometry, uint32_t color, SkRect shapeGeometry, std::shared_ptr<RSSurfaceNode> surfaceNode)
 {
     auto x = surfaceGeometry.x();
@@ -53,22 +67,25 @@ void DrawSurface(
     surfaceNode->SetBounds(x, y, width, height);
     std::shared_ptr<RSSurface> rsSurface = RSSurfaceExtractor::ExtractRSSurface(surfaceNode);
     if (rsSurface == nullptr) {
-        return;
+        return wrongExit;
     }
+#ifdef ACE_ENABLE_GPU
     if (rc_) {
         rsSurface->SetRenderContext(rc_);
     }
+#endif
     auto frame = rsSurface->RequestFrame(width, height);
     framePtr = std::move(frame);
     if (!framePtr) {
         printf("DrawSurface frameptr is nullptr");
-        return;
+        return wrongExit;
     }
     auto canvas = framePtr->GetCanvas();
     if (!canvas) {
         printf("DrawSurface canvas is nullptr");
-        return;
+        return wrongExit;
     }
+    canvas->clear(SK_ColorTRANSPARENT);
     SkPaint paint;
     paint.setAntiAlias(true);
     paint.setStyle(SkPaint::kFill_Style);
@@ -80,6 +97,52 @@ void DrawSurface(
     framePtr->SetDamageRegion(0, 0, width, height);
     auto framePtr1 = std::move(framePtr);
     rsSurface->FlushFrame(framePtr1);
+    return successExit;
+}
+
+bool DrawSurfaceCircle(
+    SkRect surfaceGeometry, float bufferW, float bufferH, uint32_t color, Circle circle, std::shared_ptr<RSSurfaceNode> surfaceNode)
+{
+    auto x = surfaceGeometry.x();
+    auto y = surfaceGeometry.y();
+    auto width = surfaceGeometry.width();
+    auto height = surfaceGeometry.height();
+    printf("[%f %f %f %f]\n", x, y, width, height);
+    printf("[%f %f %f]\n", circle.x_, circle.y_, circle.r_);
+    surfaceNode->SetBounds(x, y, width, height);
+    std::shared_ptr<RSSurface> rsSurface = RSSurfaceExtractor::ExtractRSSurface(surfaceNode);
+    if (rsSurface == nullptr) {
+        return wrongExit;
+    }
+#ifdef ACE_ENABLE_GPU
+    if (rc_) {
+        rsSurface->SetRenderContext(rc_);
+    }
+#endif
+    auto frame = rsSurface->RequestFrame(bufferW, bufferH);
+    framePtr = std::move(frame);
+    if (!framePtr) {
+        printf("DrawSurface frameptr is nullptr");
+        return wrongExit;
+    }
+    auto canvas = framePtr->GetCanvas();
+    if (!canvas) {
+        printf("DrawSurface canvas is nullptr");
+        return wrongExit;
+    }
+    canvas->clear(SK_ColorTRANSPARENT);
+    SkPaint paint;
+    paint.setAntiAlias(true);
+    paint.setStyle(SkPaint::kFill_Style);
+    paint.setStrokeWidth(20);
+    paint.setStrokeJoin(SkPaint::kRound_Join);
+    paint.setColor(color);
+
+    canvas->drawCircle(circle.x_, circle.y_, circle.r_, paint);
+    framePtr->SetDamageRegion(0, 0, width, height);
+    auto framePtr1 = std::move(framePtr);
+    rsSurface->FlushFrame(framePtr1);
+    return successExit;
 }
 
 std::shared_ptr<RSSurfaceNode> CreateSurface()
@@ -141,7 +204,7 @@ public:
     {
         std::lock_guard<std::recursive_mutex> lock(mutex_);
         if (displays_.count(id) == 0) {
-            cout << "MyDMS: No display " << id << "!" << endl;
+            std::cout << "MyDMS: No display " << id << "!" << std::endl;
             return {};
         }
         return displays_.at(id).activeMode;
@@ -151,21 +214,21 @@ public:
     {
         std::lock_guard<std::recursive_mutex> lock(mutex_);
         displays_[id] = Display { id, rsInterface_.GetScreenActiveMode(id) };
-        std::cout << "MyDMS: Display " << id << " connected." << endl;
+        std::cout << "MyDMS: Display " << id << " connected." << std::endl;
     }
 
     void OnDisplayDisConnected(ScreenId id)
     {
         std::lock_guard<std::recursive_mutex> lock(mutex_);
         if (displays_.count(id) == 0) {
-            cout << "MyDMS: No display " << id << "!" << endl;
+            std::cout << "MyDMS: No display " << id << "!" << std::endl;
         } else {
-            std::cout << "MyDMS: Display " << id << " disconnected." << endl;
+            std::cout << "MyDMS: Display " << id << " disconnected." << std::endl;
             displays_.erase(id);
             if (id == defaultDisplayId_) {
                 defaultDisplayId_ = rsInterface_.GetDefaultScreenId();
                 std::cout << "MyDMS: DefaultDisplayId changed, new DefaultDisplayId is" << defaultDisplayId_ << "."
-                          << endl;
+                          << std::endl;
             }
         }
     }
@@ -174,46 +237,64 @@ MyDMS g_dms;
 
 int main()
 {
+    std::cout << "Render service Client rs Demo.cpp start\n";
+#ifdef ACE_ENABLE_GPU
+    std::cout << "ace_enable_gpu is enabled\n";
+#else
+    std::cout << "ace_enable_gpu is disabled\n";
+#endif
     DisplayId id = g_dms.GetDefaultDisplayId();
-    cout << "RS default screen id is " << id << ".\n";
+    std::cout << "RS default screen id is " << id << ".\n";
 
     auto activeModeInfo = g_dms.GetDisplayActiveMode(id);
+    int screenWidth = 0;
+    int screenheight = 0;
     if (activeModeInfo) {
-        cout << "Display " << id << " active mode info:\n";
-        cout << "Width: " << activeModeInfo->GetScreenWidth() << ", Height: " << activeModeInfo->GetScreenHeight();
-        cout << ", FreshRate: " << activeModeInfo->GetScreenFreshRate() << "Hz.\n";
+        screenWidth = activeModeInfo->GetScreenWidth();
+        screenheight = activeModeInfo->GetScreenHeight();
+        std::cout << "Display " << id << " active mode info:\n";
+        std::cout << "Width: " << screenWidth << ", Height: " << screenheight;
+        std::cout << ", FreshRate: " << activeModeInfo->GetScreenFreshRate() << "Hz.\n";
     } else {
-        cout << "Display " << id << " has no active mode!\n";
+        std::cout << "Display " << id << " has no active mode!\n";
+        return 0;
     }
-    cout << "-------------------------------------------------------" << endl;
+    std::cout << "-------------------------------------------------------\n";
 
     auto surfaceLauncher = CreateSurface();
     auto surfaceNode1 = CreateSurface();
     surfaceNode1->SetAlpha(0.5);
     auto surfaceNode2 = CreateSurface();
     surfaceNode2->SetAlpha(0.3);
+#ifdef ACE_ENABLE_GPU
     rc_ = RenderContextFactory::GetInstance().CreateEngine();
     if (rc_) {
         rc_->InitializeEglContext();
     }
-    DrawSurface(SkRect::MakeXYWH(0, 0, 2800, 1600), 0xffffe4c4, SkRect::MakeXYWH(0, 0, 2800, 1600), surfaceLauncher);
-    DrawSurface(SkRect::MakeXYWH(80, 80, 200, 200), 0xffff0000, SkRect::MakeXYWH(0, 0, 200, 200), surfaceNode1);
-    DrawSurface(SkRect::MakeXYWH(300, 300, 200, 200), 0xff00ff00, SkRect::MakeXYWH(40, 40, 150, 150), surfaceNode2);
+#endif
+    DrawSurface(SkRect::MakeXYWH(0, 0, screenWidth, screenheight), 
+        0xffffe4c4, SkRect::MakeXYWH(0, 0, screenWidth, screenheight), surfaceLauncher);
+
+    DrawSurfaceCircle(SkRect::MakeXYWH(0, 0, screenWidth * 0.2f, screenWidth * 0.2f), screenWidth * 0.3f, screenWidth * 0.3f, 0xffff0000,
+        Circle(screenWidth * 0.15f, screenWidth * 0.15f, screenWidth * 0.12f), surfaceNode1);
+
+    DrawSurfaceCircle(SkRect::MakeXYWH(screenWidth * 0.2f, screenWidth * 0.2f, screenWidth * 0.2f, screenWidth * 0.2f), screenWidth * 0.15f, screenWidth * 0.15f, 0xff3f0033,
+        Circle(screenWidth * 0.075f, screenWidth * 0.075f, screenWidth * 0.075f), surfaceNode2);
     RSDisplayNodeConfig config;
     RSDisplayNode::SharedPtr displayNode = RSDisplayNode::Create(config);
     displayNode->AddChild(surfaceLauncher, -1);
     displayNode->AddChild(surfaceNode1, -1);
     displayNode->AddChild(surfaceNode2, -1);
     auto transactionProxy = RSTransactionProxy::GetInstance();
+    std::cout << "Render service Client rs Demo.cpp wait for 100s\n";
+    sleep(100);
     if (transactionProxy != nullptr) {
         transactionProxy->FlushImplicitTransaction();
     }
-    sleep(4);
     int positionX = 80;
     int positionY = 80;
     while (1) {
         displayNode->RemoveChild(surfaceNode1);
-        auto transactionProxy = RSTransactionProxy::GetInstance();
         if (transactionProxy != nullptr) {
             transactionProxy->FlushImplicitTransaction();
         }
@@ -236,5 +317,6 @@ int main()
         }
         sleep(4);
     }
+    std::cout << "Render service Client rs Demo.cpp end, test success\n";
     return 0;
 }

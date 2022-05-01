b9e9eaa39c1f834246f15428002513fcd3a834f0
jixingchun <jixingchun@huawei.com>
2022-05-01 20:57:14 +0800

    adapt sync_fence
    Change-Id: I8cb2889c496255ab7205067452692f0ca0489e2c
    Signed-off-by: jixingchun <jixingchun@huawei.com>

---
+++ utils/sync_fence/src/sync_fence_mingw.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "sync_fence.h"
+
+#include <unistd.h>
+#include <errno.h>
+#include <securec.h>
+
+#include "hilog/log.h"
+
+namespace OHOS {
+using namespace OHOS::HiviewDFX;
+
+namespace {
+constexpr HiLogLabel LABEL = { LOG_CORE, 0xD001400, "SyncFence" };
+constexpr int32_t INVALID_FD = -1;
+constexpr int64_t INVALID_TIMESTAMP = -1;
+
+#define SYNC_IOC_FILE_INFO _IOWR(SYNC_IOC_MAGIC, 4, struct SyncFileInfo)
+#define fcntl(...) -1
+}  // namespace
+
+const sptr<SyncFence> SyncFence::INVALID_FENCE = sptr<SyncFence>(new SyncFence(INVALID_FD));
+
+SyncFence::SyncFence(int32_t fenceFd) : fenceFd_(fenceFd)
+{
+    HiLog::Debug(LABEL, "%{public}s fenceFd: %{public}d", __func__, fenceFd_.Get());
+}
+
+SyncFence::~SyncFence()
+{
+}
+
+int32_t SyncFence::Wait(uint32_t timeout)
+{
+    (void)timeout;
+    return 0;
+}
+
+sptr<SyncFence> SyncFence::MergeFence(const std::string &name,
+                const sptr<SyncFence>& fence1, const sptr<SyncFence>& fence2)
+{
+    (void)name;
+    (void)fence1;
+    (void)fence2;
+    return INVALID_FENCE;
+}
+
+int64_t SyncFence::SyncFileReadTimestamp()
+{
+    return INVALID_TIMESTAMP;
+}
+
+int32_t SyncFence::Dup() const
+{
+    return ::dup(fenceFd_);
+}
+
+int32_t SyncFence::Get() const
+{
+    return fenceFd_;
+}
+
+void SyncFence::ReadFromMessageParcel(MessageParcel &parcel)
+{
+    int32_t fence = parcel.ReadInt32();
+    if (fence < 0) {
+        HiLog::Warn(LABEL, "ReadFromMessageParcel fence is invalid : %{public}d", fence);
+        return;
+    }
+
+    fence = parcel.ReadFileDescriptor();
+    fenceFd_ = UniqueFd(fence);
+}
+
+void SyncFence::WriteToMessageParcel(MessageParcel &parcel)
+{
+    int32_t fence = fenceFd_;
+    if (fence >= 0 && fcntl(fence, F_GETFL) == -1 && errno == EBADF) {
+        fence = -1;
+    }
+
+    parcel.WriteInt32(fence);
+
+    if (fence < 0) {
+        HiLog::Warn(LABEL, "WriteToMessageParcel fence is invalid : %{public}d", fence);
+        return;
+    }
+
+    parcel.WriteFileDescriptor(fence);
+}
+} // namespace OHOS

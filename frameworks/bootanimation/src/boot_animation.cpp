bd43f5bcdba519e680a73247aed0615d0e65216d
zengqingyu <zengqingyu3@huawei.com>
2022-05-10 16:10:27 +0800

    change time to set params in boot animation
    Signed-off-by: zengqingyu <zengqingyu3@huawei.com>
    Change-Id: I3a9c091d83d072a0bb98f4b7a09ee278c230294c

--- frameworks/bootanimation/src/boot_animation.cpp
+++ frameworks/bootanimation/src/boot_animation.cpp
@@ -136,6 +136,11 @@ void BootAnimation::Init(int32_t width, int32_t height, const std::shared_ptr<Ap
     } else {
         LOGI("SetVSyncRate success: %{public}d %{public}d", freq_, changefreq);
     }
+    if (!setBootEvent_) {
+        LOGI("set bootevent parameter");
+        system::SetParameter("bootevent.bootanimation.started", "true");
+        setBootEvent_ = true;
+    }
 }
 
 void BootAnimation::InitBootWindow()
@@ -207,11 +212,6 @@ void BootAnimation::OnVsync()
 void BootAnimation::CheckExitAnimation()
 {
     LOGI("CheckExitAnimation enter");
-    if (!setBootEvent_) {
-        LOGI("CheckExitAnimation set bootevent parameter");
-        system::SetParameter("bootevent.bootanimation.started", "true");
-        setBootEvent_ = true;
-    }
     std::string windowInit = system::GetParameter("persist.window.boot.inited", "0");
     if (windowInit == "1") {
         PostTask(std::bind(&AppExecFwk::EventRunner::Stop, runner_));

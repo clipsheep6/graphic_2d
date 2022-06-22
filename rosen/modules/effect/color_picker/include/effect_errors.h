d6384cbed99106962f9c17bb794bd65b41bb2880
wangmiaoliang <wangmiaoliang@huawei.com>
2022-06-22 10:40:39 +0800

    effectKit.ColorPicker.GetMainColor3
    Signed-off-by: wangmiaoliang <wangmiaoliang@huawei.com>
    Change-Id: Id4bee203b0d7952c40d20707a26d3cdc3c2f6814

--- rosen/modules/effect/color_picker/include/effect_errors.h
+++ rosen/modules/effect/color_picker/include/effect_errors.h
@@ -21,11 +21,10 @@
 
 namespace OHOS {
 namespace Rosen {
-
 /* Effect defined errors */
-const uint32_t ERR_EFFECT_INVALID_VALUE = 2;                                  // invalid size
-const uint32_t ERROR = 1;                                                     // Operation error
-const uint32_t SUCCESS = 0;                                                    // Operation success
+const uint32_t ERR_EFFECT_INVALID_VALUE = 2;                          // Invalid value
+const uint32_t ERROR = 1;                                             // Operation error
+const uint32_t SUCCESS = 0;                                           // Operation success
 }  // namespace Rosen
 }  // namespace OHOS
 #endif  // EFFECT_ERRORS_H

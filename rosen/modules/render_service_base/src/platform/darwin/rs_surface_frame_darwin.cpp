bb3991a53d1e57076674f9896e0680d64ff59ba6
liyu <liyu37@huawei.com>
2022-10-14 15:12:22 +0800

    * implement render_service_base/platform/darwin
    Change-Id: Iccce45ab2dcb0b632558edb804b8c9190c527a9b
    Signed-off-by: liyu <liyu37@huawei.com>

---
+++ rosen/modules/render_service_base/src/platform/darwin/rs_surface_frame_darwin.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rs_surface_frame_darwin.h"
+
+#include "platform/common/rs_log.h"
+
+namespace OHOS {
+namespace Rosen {
+RSSurfaceFrameDarwin::RSSurfaceFrameDarwin(int32_t width, int32_t height)
+{
+    requestConfig_ = {
+        .width = width,
+        .height = height,
+        .strideAlignment = 0x8,
+        .format = PIXEL_FMT_RGBA_8888,
+        .usage = HBM_USE_CPU_READ | HBM_USE_CPU_WRITE | HBM_USE_MEM_DMA,
+        .timeout = 0,
+    };
+    flushConfig_ = {
+        .damage = {
+            .x = 0,
+            .y = 0,
+            .w = width,
+            .h = height,
+        },
+    };
+}
+
+void RSSurfaceFrameDarwin::SetDamageRegion(int32_t left, int32_t top, int32_t width, int32_t height)
+{
+    flushConfig_.damage.x = left;
+    flushConfig_.damage.y = top;
+    flushConfig_.damage.w = width;
+    flushConfig_.damage.h = height;
+}
+
+int32_t RSSurfaceFrameDarwin::GetBufferAge() const
+{
+    return -1;
+}
+
+SkCanvas* RSSurfaceFrameDarwin::GetCanvas()
+{
+    if (surface_ == nullptr) {
+        CreateSurface();
+    }
+
+    if (surface_ != nullptr) {
+        return surface_->getCanvas();
+    }
+    return nullptr;
+}
+
+sk_sp<SkSurface> RSSurfaceFrameDarwin::GetSurface()
+{
+    return surface_;
+}
+
+void RSSurfaceFrameDarwin::SetRenderContext(RenderContext* context)
+{
+    renderContext_ = context;
+}
+
+void RSSurfaceFrameDarwin::CreateSurface()
+{
+    if (buffer_ == nullptr) {
+        ROSEN_LOGW("RSSurfaceFrameDarwin::GetCanvas buffer is nullptr");
+        return;
+    }
+
+    const auto &addr = reinterpret_cast<uint32_t *>(buffer_->GetVirAddr());
+    if (addr == nullptr) {
+        ROSEN_LOGW("RSSurfaceFrameDarwin::GetCanvas buffer.addr is nullptr");
+        return;
+    }
+
+    const auto &width = buffer_->GetWidth();
+    const auto &height = buffer_->GetHeight();
+    const auto &size = buffer_->GetSize();
+    const auto &info = SkImageInfo::Make(width, height, kRGBA_8888_SkColorType, kPremul_SkAlphaType);
+    surface_ = SkSurface::MakeRasterDirect(info, addr, size / height);
+}
+} // namespace Rosen
+} // namespace OHOS

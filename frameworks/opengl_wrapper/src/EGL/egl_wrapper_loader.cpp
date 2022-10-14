50492670d61b3875d1643d737ce931ff60a8e164
Mupceet <laiguizhong@huawei.com>
2022-10-14 15:50:50 +0800

    chipset
    Signed-off-by: Mupceet <laiguizhong@huawei.com>
    Change-Id: I7989e9e863722b5b4034705691464a0ba87d6fcb

--- frameworks/opengl_wrapper/src/EGL/egl_wrapper_loader.cpp
+++ frameworks/opengl_wrapper/src/EGL/egl_wrapper_loader.cpp
@@ -27,10 +27,10 @@ using namespace OHOS;
 namespace OHOS {
 namespace {
 #ifdef __aarch64__
-    constexpr const char *VENDOR_LIB_PATH = "/vendor/lib64/chipsetsdk/";
+    constexpr const char *VENDOR_LIB_PATH = "/chipset/lib64/chipsetsdk/";
     constexpr const char *SYSTEM_LIB_PATH = "/system/lib64/";
 #else
-    constexpr const char *VENDOR_LIB_PATH = "/vendor/lib/chipsetsdk/";
+    constexpr const char *VENDOR_LIB_PATH = "/chipset/lib/chipsetsdk/";
     constexpr const char *SYSTEM_LIB_PATH = "/system/lib/";
 #endif
 constexpr const char *LIB_EGL_NAME = "libEGL_impl.so";

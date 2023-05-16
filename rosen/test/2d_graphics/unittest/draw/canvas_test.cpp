c4d9d0cd7545b47da7def7d49531f503b09124f5
huangzejia1 <huangzejia1@huawei.com>
2023-05-16 14:40:47 +0800

    fix comment
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>
    Change-Id: Ib77c6f4c514b9424811a51a63f12537b56303874

--- rosen/test/2d_graphics/unittest/draw/canvas_test.cpp
+++ rosen/test/2d_graphics/unittest/draw/canvas_test.cpp
@@ -311,10 +311,9 @@ HWTEST_F(CanvasTest, CanvasDrawShadowTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasDrawRegionTest001
- * @tc.desc:
+ * @tc.desc: Test for drawing Region on the Canvas.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasDrawRegionTest001, TestSize.Level1)
 {
@@ -326,10 +325,9 @@ HWTEST_F(CanvasTest, CanvasDrawRegionTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasDrawBitmapTest001
- * @tc.desc:
+ * @tc.desc: Test for drawing Bitmap on the Canvas.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasDrawBitmapTest001, TestSize.Level1)
 {
@@ -341,10 +339,9 @@ HWTEST_F(CanvasTest, CanvasDrawBitmapTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasDrawImageTest001
- * @tc.desc:
+ * @tc.desc: Test for drawing image on the Canvas.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasDrawImageTest001, TestSize.Level1)
 {
@@ -357,10 +354,9 @@ HWTEST_F(CanvasTest, CanvasDrawImageTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasDrawImageRectTest001
- * @tc.desc:
+ * @tc.desc: Test for DrawImageRect function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasDrawImageRectTest001, TestSize.Level1)
 {
@@ -375,10 +371,9 @@ HWTEST_F(CanvasTest, CanvasDrawImageRectTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasDrawImageRectTest002
- * @tc.desc:
+ * @tc.desc: Test for DrawImageRect function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasDrawImageRectTest002, TestSize.Level1)
 {
@@ -392,10 +387,9 @@ HWTEST_F(CanvasTest, CanvasDrawImageRectTest002, TestSize.Level1)
 
 /**
  * @tc.name: CanvasDrawPictureTest001
- * @tc.desc:
+ * @tc.desc: Test for DrawPicture function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasDrawPictureTest001, TestSize.Level1)
 {
@@ -407,10 +401,9 @@ HWTEST_F(CanvasTest, CanvasDrawPictureTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasClipRectTest001
- * @tc.desc:
+ * @tc.desc: Test replacing the clipping area with the intersection or difference between clipping area and Rect.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasClipRectTest001, TestSize.Level1)
 {
@@ -422,10 +415,9 @@ HWTEST_F(CanvasTest, CanvasClipRectTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasClipRoundRectTest001
- * @tc.desc:
+ * @tc.desc: Test replacing the clipping area with the intersection or difference of clipping area and Rect.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasClipRoundRectTest001, TestSize.Level1)
 {
@@ -438,10 +430,9 @@ HWTEST_F(CanvasTest, CanvasClipRoundRectTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasClipPathTest001
- * @tc.desc:
+ * @tc.desc: Test replacing the clipping area with the intersection or difference between clipping area and path.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasClipPathTest001, TestSize.Level1)
 {
@@ -453,10 +444,9 @@ HWTEST_F(CanvasTest, CanvasClipPathTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasSetMatrixTest001
- * @tc.desc:
+ * @tc.desc: Test for SetMatrix function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasSetMatrixTest001, TestSize.Level1)
 {
@@ -468,10 +458,9 @@ HWTEST_F(CanvasTest, CanvasSetMatrixTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasResetMatrixTest001
- * @tc.desc:
+ * @tc.desc: Test for ResetMatrix function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasResetMatrixTest001, TestSize.Level1)
 {
@@ -484,10 +473,9 @@ HWTEST_F(CanvasTest, CanvasResetMatrixTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasConcatMatrixTest001
- * @tc.desc:
+ * @tc.desc: Test for ConcatMatrix function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasConcatMatrixTest001, TestSize.Level1)
 {
@@ -499,10 +487,9 @@ HWTEST_F(CanvasTest, CanvasConcatMatrixTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasTranslateTest001
- * @tc.desc:
+ * @tc.desc: Test for Translate function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasTranslateTest001, TestSize.Level1)
 {
@@ -513,10 +500,9 @@ HWTEST_F(CanvasTest, CanvasTranslateTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasScaleTest001
- * @tc.desc:
+ * @tc.desc: Test for Scale function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasScaleTest001, TestSize.Level1)
 {
@@ -527,10 +513,9 @@ HWTEST_F(CanvasTest, CanvasScaleTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasRotateTest001
- * @tc.desc:
+ * @tc.desc: Test for Rotating Matrix by degrees.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasRotateTest001, TestSize.Level1)
 {
@@ -541,10 +526,9 @@ HWTEST_F(CanvasTest, CanvasRotateTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasRotateTest002
- * @tc.desc:
+ * @tc.desc: Test for Rotating Matrix by degrees.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasRotateTest002, TestSize.Level1)
 {
@@ -555,10 +539,9 @@ HWTEST_F(CanvasTest, CanvasRotateTest002, TestSize.Level1)
 
 /**
  * @tc.name: CanvasShearTest001
- * @tc.desc:
+ * @tc.desc: Test for Shear function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasShearTest001, TestSize.Level1)
 {
@@ -569,10 +552,9 @@ HWTEST_F(CanvasTest, CanvasShearTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasFlushTest001
- * @tc.desc:
+ * @tc.desc: Test for Flush function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasFlushTest001, TestSize.Level1)
 {
@@ -583,10 +565,9 @@ HWTEST_F(CanvasTest, CanvasFlushTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasClearTest001
- * @tc.desc:
+ * @tc.desc: Test for Clear function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasClearTest001, TestSize.Level1)
 {
@@ -597,10 +578,9 @@ HWTEST_F(CanvasTest, CanvasClearTest001, TestSize.Level1)
 
 /**
  * @tc.name: CanvasSaveTest001
- * @tc.desc:
+ * @tc.desc: Test for Save function.
  * @tc.type: FUNC
- * @tc.require: AR000GGNV3
- * @tc.author:
+ * @tc.require: I719R9
  */
 HWTEST_F(CanvasTest, CanvasSaveTest001, TestSize.Level1)
 {

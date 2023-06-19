35b71d807ecd72cab5ba90881ecd5053209027b1
h00574155 <hujunjian1@huawei.com>
2023-06-19 06:24:00 +0000

    Add hdi display capability info in Composer
    Signed-off-by: h00574155 <hujunjian1@huawei.com>
    Change-Id: I08886a280b448b723b8e396093f4ea7894be65af

--- rosen/modules/render_service_client/test/render_service_client_screen_mode_demo.cpp
+++ rosen/modules/render_service_client/test/render_service_client_screen_mode_demo.cpp
@@ -33,37 +33,31 @@ public:
     void TestRun()
     {
         std::cout << "Render service Client screen mode Demo.cpp testRun Start\n";
-        std::cout << "The default screen id is " << defaultDisplayId_ << std::endl;
-        while (true) {
-            decltype(supportedModes_.size()) size = supportedModes_.size();
-            if (size == 0) {
-                std::cout << "The default screen id is " << defaultDisplayId_ <<
-                    ", and it dosen't spupport any screen mode." << std::endl;
-                break;
-            }
+        decltype(supportedModes_.size()) size = supportedModes_.size();
+        if (size == 0) {
             std::cout << "The default screen id is " << defaultDisplayId_ <<
-                ", and it spupports these screen modes:." << std::endl;
-            for (decltype(supportedModes_.size()) i = 0; i < size; i++) {
-                std::cout << "supporedMode[" << i << "]: " << supportedModes_[i].GetScreenModeId() <<", " <<
-                    supportedModes_[i].GetScreenWidth() << "x" << supportedModes_[i].GetScreenHeight() <<
-                    ", refreshrate=" << supportedModes_[i].GetScreenRefreshRate() << std::endl;
-            }
-            RSScreenModeInfo currScreenMode = rsInterface_.GetScreenActiveMode(defaultDisplayId_);
-            std::cout << "The current active screen mode :\n modeId: " << currScreenMode.GetScreenModeId() <<", " <<
-                currScreenMode.GetScreenWidth() << "x" << currScreenMode.GetScreenHeight() <<
-                ", refreshrate=" << currScreenMode.GetScreenRefreshRate() << std::endl;
-
-            bool ret = ChangeScreenActiveMode();
-            if (!ret) {
-                break;
-            }
+                ", and it dosen't spupport any screen mode." << std::endl;
+            return;
         }
-
-        rsInterface_.SetScreenActiveMode(defaultDisplayId_, defaultModeId_);
+        std::cout << "The default screen id is " << defaultDisplayId_ <<
+            ", and it spupports these screen modes:." << std::endl;
+        for (decltype(supportedModes_.size()) i = 0; i < size; i++) {
+            std::cout << "supporedMode[" << i << "]: modeId=" << supportedModes_[i].GetScreenModeId() <<
+                ", phyWidth*phyHeight=" << supportedModes_[i].GetScreenWidth() << "*" <<
+                supportedModes_[i].GetScreenHeight() <<
+                ", refreshrate=" << supportedModes_[i].GetScreenRefreshRate() << std::endl;
+        }
         RSScreenModeInfo currScreenMode = rsInterface_.GetScreenActiveMode(defaultDisplayId_);
-        std::cout << "The current active screen mode :\n modeId: " << currScreenMode.GetScreenModeId() <<", " <<
-            currScreenMode.GetScreenWidth() << "x" << currScreenMode.GetScreenHeight() <<
+        std::cout << "The current active screen mode :\n modeIndex: " << currScreenMode.GetScreenModeId() <<
+            ", phyWidth*phyHeight=" << currScreenMode.GetScreenWidth() << "*" << currScreenMode.GetScreenHeight() <<
             ", refreshrate=" << currScreenMode.GetScreenRefreshRate() << std::endl;
+
+        ChangeScreenActiveMode();
+        currScreenMode = rsInterface_.GetScreenActiveMode(defaultDisplayId_);
+        std::cout << "The current active screen mode :\n modeIndex: " << currScreenMode.GetScreenModeId() <<
+            ", phyWidth*phyHeight=" << currScreenMode.GetScreenWidth() << "*" << currScreenMode.GetScreenHeight() <<
+            ", refreshrate=" << currScreenMode.GetScreenRefreshRate() << std::endl;
+
         std::cout << "Render service Client screen mode Demo.cpp testRun end\n";
     }
 
@@ -73,43 +67,25 @@ private:
     {
         defaultDisplayId_ = rsInterface_.GetDefaultScreenId();
         supportedModes_ = rsInterface_.GetScreenSupportedModes(defaultDisplayId_);
-        RSScreenModeInfo defaultScreenMode = rsInterface_.GetScreenActiveMode(defaultDisplayId_);
-        defaultModeId_ = defaultScreenMode.GetScreenModeId();
     };
 
-    bool ChangeScreenActiveMode()
+    void ChangeScreenActiveMode()
     {
         std::string inputStr;
-        std::cout << "\n\nPlase input screen mode id:\n";
+        std::cout << "\n\nPlase input screen mode index:\n";
         std::cin >> inputStr;
         uint32_t newScreenModeId = 0;
         for (auto iter = inputStr.begin(); iter != inputStr.end(); iter++) {
-            if (*iter < '0' || *iter > '9') {
-                return false;
-            }
             newScreenModeId *= 10; // 10 : decimal
             newScreenModeId += (*iter - '0');
         }
-
-        decltype(supportedModes_.size()) size = supportedModes_.size();
-        if (newScreenModeId >= size) {
-            std::cout << "The new screen mode id is " << newScreenModeId << ", please input correct value[0";
-            if (size == 1) {
-                std::cout << "]\n\n";
-            } else {
-                std::cout << "-" << (size - 1) << "]\n\n";
-            }
-        } else {
-            std::cout << "The new screen mode id is " << newScreenModeId << "\n\n";
-            rsInterface_.SetScreenActiveMode(defaultDisplayId_, newScreenModeId);
-        }
-        return true;
+        std::cout << "The new screen mode index is " << newScreenModeId << "\n\n";
+        rsInterface_.SetScreenActiveMode(defaultDisplayId_, newScreenModeId);
     }
 
     RSInterfaces& rsInterface_;
     ScreenId defaultDisplayId_ = 0;
     std::vector<RSScreenModeInfo> supportedModes_;
-    uint32_t defaultModeId_ = 0;
 };
 
 int main()

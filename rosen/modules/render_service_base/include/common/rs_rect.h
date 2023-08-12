259e02d0ac901e9f0920f6bd8ef5cfbc9de5e379
c00607827 <cenyouyi@huawei.com>
2023-08-12 15:31:19 +0800

    modify log
    Signed-off-by: c00607827 <cenyouyi@huawei.com>

--- rosen/modules/render_service_base/include/common/rs_rect.h
+++ rosen/modules/render_service_base/include/common/rs_rect.h
@@ -21,7 +21,6 @@
 #include "common/rs_common_def.h"
 #include "common/rs_vector2.h"
 #include "common/rs_vector4.h"
-#include "platform/common/rs_log.h"
 #include "transaction/rs_marshalling_helper.h"
 
 namespace OHOS {
@@ -197,7 +196,6 @@ public:
                 RSMarshallingHelper::Marshalling(parcel, top_) &&
                 RSMarshallingHelper::Marshalling(parcel, width_) &&
                 RSMarshallingHelper::Marshalling(parcel, height_))) {
-            ROSEN_LOGE("RectT::Marshalling failed!");
             return false;
         }
         return true;
@@ -210,7 +208,6 @@ public:
                 RSMarshallingHelper::Unmarshalling(parcel, rect->top_) &&
                 RSMarshallingHelper::Unmarshalling(parcel, rect->width_) &&
                 RSMarshallingHelper::Unmarshalling(parcel, rect->height_))) {
-            ROSEN_LOGE("RectT::Unmarshalling failed!");
             return nullptr;
         }
         return rect.release();

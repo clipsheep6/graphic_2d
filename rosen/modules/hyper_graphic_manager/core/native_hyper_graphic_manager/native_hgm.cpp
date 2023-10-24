cda09d02f0ca505f27961062a0c94f657b45f1f9
xuweinan <xuweinan5@huawei.com>
2023-10-24 16:09:04 +0800

    Description:Hgm NDK interface provide Feature or Bugfix:Feature Binary Source:No
    Signed-off-by: xuweinan <xuweinan5@huawei.com>

---
+++ rosen/modules/hyper_graphic_manager/core/native_hyper_graphic_manager/native_hgm.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "native_hgm.h"
+
+#include <stdint.h>
+#include "transaction/rs_interfaces.h"
+#include "hgm_log.h"
+#include "hgm_command.h"
+
+namespace OHOS::Rosen{
+
+    int OH_NativeHgm_GetScreenCurrentRefreshRate(int32_t screen)
+    {
+        ScreenId id = 0;
+        if(screen < 0){
+            HGM_LOGE("Native HGM GetScreenCurrentRefreshRate illegal screenId");
+        }else{
+            id = static_cast<ScreenId>(screen);
+        }
+        auto& interfaces = OHOS::Rosen::RSInterfaces::GetInstance();
+        uint32_t currentRate = interfaces.GetScreenCurrentRefreshRate(id);
+        int32_t returnInt = 0;
+        if (currentRate > static_cast<uint32_t>(OLED_MAX_HZ)) {
+          HGM_LOGE("Native HGM  GetScreenCurrentRefreshRate got a rate too above maximal value");
+        } else {
+            returnInt = static_cast<int32_t>(currentRate);
+        }
+        return returnInt;
+    }
+}

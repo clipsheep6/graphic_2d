6096fa748a58e1a432b0aab0773bca13da23197f
linjiaxin <linjiaxin7@huawei.com>
2023-10-26 09:57:32 +0000

    bugfix
    Signed-off-by: linjiaxin <linjiaxin7@huawei.com>

--- rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
@@ -220,11 +220,11 @@ void RSCanvasRenderNode::ProcessAnimatePropertyAfterChildren(RSPaintFilterCanvas
     RSModifierContext context = { GetMutableRenderProperties(), &canvas };
     ApplyDrawCmdModifier(context, RSModifierType::FOREGROUND_STYLE);
     RSPropertiesPainter::DrawColorFilter(GetRenderProperties(), canvas);
+
     canvas.RestoreStatus(canvasNodeSaveCount_);
     if (GetRenderProperties().IsLightUpEffectValid()) {
         RSPropertiesPainter::DrawLightUpEffect(GetRenderProperties(), canvas);
     }
-
     RSPropertiesPainter::DrawFilter(GetRenderProperties(), canvas, FilterType::FOREGROUND_FILTER);
     auto para = GetRenderProperties().GetLinearGradientBlurPara();
     RSPropertiesPainter::DrawLinearGradientBlurFilter(GetRenderProperties(), canvas);

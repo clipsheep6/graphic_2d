7e359c6567c7a90ab28d2bf10ec9408d28d432ad
xuweinan <xuweinan5@huawei.com>
2023-11-06 15:13:55 +0800

    Description:Hgm NDK interface add Feature or Bugfix:Feature
    Signed-off-by: xuweinan <xuweinan5@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/native_hyper_graphic_manager/native_hgm.cpp
+++ rosen/modules/hyper_graphic_manager/core/native_hyper_graphic_manager/native_hgm.cpp
@@ -16,28 +16,62 @@
 #include "native_hgm.h"
 
 #include <stdint.h>
+#include <vector>
+#include <cstdio>
 #include "transaction/rs_interfaces.h"
 #include "hgm_log.h"
 #include "hgm_command.h"
+namespace OHOS::Rosen {
 
-namespace OHOS::Rosen{
-
-    int OH_NativeHgm_GetScreenCurrentRefreshRate(int32_t screen)
-    {
-        ScreenId id = 0;
-        if(screen < 0){
-            HGM_LOGE("Native HGM GetScreenCurrentRefreshRate illegal screenId");
-        }else{
-            id = static_cast<ScreenId>(screen);
-        }
-        auto& interfaces = OHOS::Rosen::RSInterfaces::GetInstance();
-        uint32_t currentRate = interfaces.GetScreenCurrentRefreshRate(id);
-        int32_t returnInt = 0;
-        if (currentRate > static_cast<uint32_t>(OLED_MAX_HZ)) {
-          HGM_LOGE("Native HGM  GetScreenCurrentRefreshRate got a rate too above maximal value");
-        } else {
-            returnInt = static_cast<int32_t>(currentRate);
-        }
-        return returnInt;
-    }
+void OH_NativeHgm_SetRefreshRateMode(int32_t mode)
+{
+    auto& interfaces = OHOS::Rosen::RSInterfaces::GetInstance();
+    interfaces.SetRefreshRateMode(mode);
 }
+
+int32_t OH_NativeHgm_GetScreenCurrentRefreshRate(int32_t screen)
+{
+    ScreenId id = 0;
+    if (screen < 0) {
+        HGM_LOGE("Native HGM GetScreenCurrentRefreshRate illegal screenId");
+    } else {
+        id = static_cast<ScreenId>(screen);
+    }
+    auto& interfaces = OHOS::Rosen::RSInterfaces::GetInstance();
+    uint32_t currentRate = interfaces.GetScreenCurrentRefreshRate(id);
+    int32_t returnInt = 0;
+    if (currentRate > static_cast<uint32_t>(OLED_MAX_HZ)) {
+        HGM_LOGE("Native HGM  GetScreenCurrentRefreshRate got a rate too above maximal value");
+    } else {
+        returnInt = static_cast<int32_t>(currentRate);
+    }
+    return returnInt;
+}
+
+int32_t OH_NativeHgm_GetCurrentRefreshRateMode()
+{
+    int32_t defaultRateMode = 0;
+    auto& interfaces = OHOS::Rosen::RSInterfaces::GetInstance();
+    defaultRateMode = interfaces.GetCurrentRefreshRateMode();
+    return defaultRateMode;
+}
+
+int32_t* OH_NativeHgm_GetScreenSupportedRefreshRates(int32_t screen)
+{
+    ScreenId id = 0;
+    if (screen < 0) {
+        HGM_LOGE("Native HGM GetScreenCurrentRefreshRate illegal screenId");
+    } else {
+        id = static_cast<ScreenId>(screen);
+    }
+    auto& interfaces = OHOS::Rosen::RSInterfaces::GetInstance();
+    std::vector<int32_t> currentRates = interfaces.GetScreenSupportedRefreshRates(id);
+    int numSupportedRates = static_cast<int>(currentRates.size());
+    int32_t *arr = new int32_t[numSupportedRates];
+    for (int index = 0; index < numSupportedRates; ++index) {
+        arr[index] = currentRates[index];
+    }
+    return arr;
+}
+
+} // namespace OHOS::Rosen

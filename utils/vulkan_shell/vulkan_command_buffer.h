d95ac50ea690a9e160149b9b8c0f306316301d23
andrew0229 <zhangzhao62@huawei.com>
2023-12-05 03:18:01 +0000

    vulkan shell for drawingdoc
    Signed-off-by: andrew0229 <zhangzhao62@huawei.com>
    Change-Id: I7057ace4a8711b90c7003925ca293a994f60e2d9

---
+++ utils/vulkan_shell/vulkan_command_buffer.h
@@ -0,0 +1,64 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_VULKAN_VULKAN_COMMAND_BUFFER_H_
+#define FLUTTER_VULKAN_VULKAN_COMMAND_BUFFER_H_
+
+#include "vulkan_handle.h"
+
+namespace vulkan {
+
+class VulkanProcTable;
+
+class VulkanCommandBuffer {
+ public:
+  VulkanCommandBuffer(const VulkanProcTable& vk,
+                      const VulkanHandle<VkDevice>& device,
+                      const VulkanHandle<VkCommandPool>& pool);
+
+  ~VulkanCommandBuffer();
+
+  bool IsValid() const;
+
+  VkCommandBuffer Handle() const;
+
+#ifndef RS_ENABLE_VK
+  FML_WARN_UNUSED_RESULT
+#endif
+  bool Begin() const;
+
+#ifndef RS_ENABLE_VK
+  FML_WARN_UNUSED_RESULT
+#endif
+  bool End() const;
+
+#ifndef RS_ENABLE_VK
+  FML_WARN_UNUSED_RESULT
+#endif
+  bool InsertPipelineBarrier(
+      VkPipelineStageFlagBits src_stage_flags,
+      VkPipelineStageFlagBits dest_stage_flags,
+      uint32_t /* mask of VkDependencyFlagBits */ dependency_flags,
+      uint32_t memory_barrier_count,
+      const VkMemoryBarrier* memory_barriers,
+      uint32_t buffer_memory_barrier_count,
+      const VkBufferMemoryBarrier* buffer_memory_barriers,
+      uint32_t image_memory_barrier_count,
+      const VkImageMemoryBarrier* image_memory_barriers) const;
+
+ private:
+  const VulkanProcTable& vk;
+  const VulkanHandle<VkDevice>& device_;
+  const VulkanHandle<VkCommandPool>& pool_;
+  VulkanHandle<VkCommandBuffer> handle_;
+  bool valid_;
+
+#ifndef RS_ENABLE_VK
+  FML_DISALLOW_COPY_AND_ASSIGN(VulkanCommandBuffer);
+#endif
+};
+
+}  // namespace vulkan
+
+#endif  // FLUTTER_VULKAN_VULKAN_COMMAND_BUFFER_H_
---
+++ utils/vulkan_shell/vulkan_surface.h
@@ -0,0 +1,48 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_VULKAN_VULKAN_SURFACE_H_
+#define FLUTTER_VULKAN_VULKAN_SURFACE_H_
+
+#include "vulkan_handle.h"
+#include "third_party/skia/include/core/SkSize.h"
+
+namespace vulkan {
+
+class VulkanProcTable;
+class VulkanApplication;
+class VulkanNativeSurface;
+
+class VulkanSurface {
+ public:
+  VulkanSurface(VulkanProcTable& vk,
+                VulkanApplication& application,
+                std::unique_ptr<VulkanNativeSurface> native_surface);
+
+  ~VulkanSurface();
+
+  bool IsValid() const;
+
+  /// Returns the current size of the surface or (0, 0) if invalid.
+  SkISize GetSize() const;
+
+  const VulkanHandle<VkSurfaceKHR>& Handle() const;
+
+  const VulkanNativeSurface& GetNativeSurface() const;
+
+ private:
+  VulkanProcTable& vk;
+  VulkanApplication& application_;
+  std::unique_ptr<VulkanNativeSurface> native_surface_;
+  VulkanHandle<VkSurfaceKHR> surface_;
+  bool valid_;
+
+#ifndef RS_ENABLE_VK
+  FML_DISALLOW_COPY_AND_ASSIGN(VulkanSurface);
+#endif
+};
+
+}  // namespace vulkan
+
+#endif  // FLUTTER_VULKAN_VULKAN_SURFACE_H_

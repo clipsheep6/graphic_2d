9c09b6f5804f7bf96d9ce8d418886e90a68306c2
dengyanfang23 <dengyanfang@huawei.com>
2024-01-16 19:40:03 +0800

    增加测试用例 Signed-off-by: dengyanfang23 <dengyanfang@huawei.com>

--- rosen/test/2d_engine/unittest/rosen_text/texgine/hm_symbol_test.cpp
+++ rosen/test/2d_engine/unittest/rosen_text/texgine/hm_symbol_test.cpp
@@ -90,5 +90,55 @@ HWTEST_F(OHHmSymbolTest, OHHmSymbolTest003, TestSize.Level1)
     SymbolLayers symbolInfo = TextEngine::HMSymbolRun::GetSymbolLayers(glyphId, textStyle.symbol);
 }
 
+/*
+ * @tc.name: OHHmSymbolTest004
+ * @tc.desc: test for symbol GetSymbolLayers
+ * @tc.type: FUNC
+ */
+HWTEST_F(OHHmSymbolTest, OHHmSymbolTest004, TestSize.Level1)
+{
+#ifndef USE_ROSEN_DRAWING
+    SkGlyphID glyphId = 3; // this 3 is glyphID of symbol
+#else
+    uint16_t glyohId = 3; // this 3 is glyphID of symbol
+#endif
+    HMSymbolTxt symbol = HMSymbolTxt();
+    Drawing::Color color1 = Drawing::Color::COLOR_BLUE;
+    Drawing::Color color2 = Drawing::Color::COLOR_GREEN;
+    Drawing::Color color3 = Drawing::Color::COLOR_RED;
+    std::vector<Drawing::Color> colors = {color1, color2, color3};
+    symbol.SetRenderColor(colors);
+    symbol.SetRenderMode(1); // this 1 is MULTIPLE_COLOR of render mode
+    symbol.SetSymbolEffect(1); // this 1 is EFFECT_SCALE of effect strategy
+
+    TextEngine::TextStyle textStyle;
+    textStyle.isSymbolGlyph = true;
+    textStyle.symbol.SetRenderColor(symbol.GetRenderColor());
+    textStyle.symbol.SetRenderMode(symbol.GetRenderMode());
+    textStyle.symbol.SetSymbolEffect(symbol.GetEffectStrategy());
+    SymbolLayers symbolInfo = TextEngine::HMSymbolRun::GetSymbolLayers(glyphId, textStyle.symbol);
+    symbol.HMSymbolTxt::~HMSymbolTxt();
+}
+
+/*
+ * @tc.name: OHHmSymbolTest005
+ * @tc.desc: test for symbol DrawSymbol
+ * @tc.type: FUNC
+ */
+HWTEST_F(OHHmSymbolTest, OHHmSymbolTest005, TestSize.Level1)
+{
+    TextEngine::TexgineCanvas canvas;
+    std::shared_ptr<TextEngine::TexgineTextBlob> blob =
+        std::make_shared<TextEngine::TexgineTextBlob>();
+    std::pair<double, double> offset = {0.0, 0.0};
+    TextEngine::TexginePaint paint;
+    TextEngine::TextStyle textStyle;
+    textStyle.isSymbolGlyph = true;
+    TextEngine::HMSymbolRun::DrawSymbol(canvas, blob, offset, paint, textStyle);
+
+    blob = nullptr;
+    TextEngine::HMSymbolRun::DrawSymbol(canvas, blob, offset, paint, textStyle);
+}
+
 } // namespace Rosen
 } // namespace OHOS

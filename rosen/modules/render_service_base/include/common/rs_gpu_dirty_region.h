8768712b5c477427d2eb5116912492e5d1ea0da1
wangchenchuan <wangchenchuan@huawei.com>
2024-01-25 16:38:21 +0800

    提供XPower获取脏区的接口
    Signed-off-by: wangchenchuan <wangchenchuan@huawei.com>

---
+++ rosen/modules/render_service_base/include/common/rs_gpu_dirty_region.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RENDER_SERVICE_CLIENT_CORE_COMMON_RS_GPU_DIRTY_REGION_H
+#define RENDER_SERVICE_CLIENT_CORE_COMMON_RS_GPU_DIRTY_REGION_H
+
+#include <unordered_map>
+
+#include "common/rs_common_def.h"
+#include "common/rs_rect.h"
+
+namespace OHOS {
+namespace Rosen {
+
+struct GpuDirtyRegionInfo {
+    int64_t activeGpuDirtyRegionInfo;
+    int64_t globalGpuDirtyRegionInfo;
+    int32_t skipProcessFramesNumber;
+    int32_t activeFramesNumber;
+    int32_t globalFramesNumber;
+    std::string windowName;
+    GpuDirtyRegionInfo()
+        : activeGpuDirtyRegionInfo(), globalGpuDirtyRegionInfo(), skipProcessFramesNumber(), activeFramesNumber(),
+          globalFramesNumber(), windowName() {}
+    GpuDirtyRegionInfo(int64_t activeGpuDirtyRegionInfo_, int64_t globalGpuDirtyRegionInfo_,
+        int32_t skipProcessFramesNumber_, int32_t activeFramesNumber_, int32_t globalFramesNumber_,
+        std::string windowName_)
+        : activeGpuDirtyRegionInfo(activeGpuDirtyRegionInfo_), globalGpuDirtyRegionInfo(globalGpuDirtyRegionInfo_),
+          skipProcessFramesNumber(skipProcessFramesNumber_), activeFramesNumber(activeFramesNumber_),
+          globalFramesNumber(globalFramesNumber_), windowName(windowName_) {}
+};
+
+class RSB_EXPORT GpuDirtyRegion {
+public:
+    static GpuDirtyRegion& GetInstance();
+
+    void UpdateActiveGpuDirtyRegionInfoAndFrameNumberForXpower(NodeId id, std::vector<RectI> rects);
+    void UpdateGlobalGpuDirtyRegionInfoAndFrameNumberForXpower(NodeId id, RectI rect);
+    void AddSkipProcessFramesNumberForXpower(NodeId id);
+    void SetWindowNameForXpower(NodeId id, std::string& windowName);
+    GpuDirtyRegionInfo GetGpuDirtyRegionInfo(NodeId id);
+    void ResetGpuDirtyRegionInfo();
+
+private:
+    GpuDirtyRegion();
+    ~GpuDirtyRegion() noexcept;
+    GpuDirtyRegion(const GpuDirtyRegion&) = delete;
+    GpuDirtyRegion(const GpuDirtyRegion&&) = delete;
+    GpuDirtyRegion& operator=(const GpuDirtyRegion&) = delete;
+    GpuDirtyRegion& operator=(const GpuDirtyRegion&&) = delete;
+
+    std::unordered_map<NodeId, GpuDirtyRegionInfo> gpuDirtyRegionInfoMap_;
+};
+} // namespace Rosen
+} // namespace OHOS
+
+#endif // RENDER_SERVICE_CLIENT_CORE_COMMON_RS_GPU_DIRTY_REGION_H

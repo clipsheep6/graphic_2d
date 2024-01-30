8a862f4ade44f901e6dfa0904c9bea26aca816cb
wangxutao <wangxutao5@huawei.com>
2024-01-30 19:50:35 +0800

    egl enable blob cache
    Signed-off-by: wangxutao <wangxutao5@huawei.com>

---
+++ frameworks/opengl_wrapper/src/EGL/egl_blob_cache.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "egl_blob_cache.h"
+namespace OHOS{
+BlobCache* BlobCache::blob_cache_;
+BlobCache* BlobCache::get() {
+    return BlobCache::blob_cache_;
+}
+
+void BlobCache::setBlobFunc(const void* key, EGLsizeiANDROID keySize, const void* value,
+                    EGLsizeiANDROID valueSize) {
+    BlobCache::get()->setBlob(key, keySize, value, valueSize);
+}
+
+EGLsizeiANDROID BlobCache::getBlobFunc(const void* key, EGLsizeiANDROID keySize, void* value,
+                               EGLsizeiANDROID valueSize) {
+    return BlobCache::get()->getBlob(key, keySize, value, valueSize);
+}
+
+void BlobCache::setBlob(const void* key, EGLsizeiANDROID keySize, const void* value,
+                    EGLsizeiANDROID valueSize) {
+}
+
+EGLsizeiANDROID BlobCache::getBlob(const void* key, EGLsizeiANDROID keySize, void* value,
+                               EGLsizeiANDROID valueSize) {
+}
+
+void BlobCache::initialize(EGLDisplay dpy); {
+    
+}
+
+}
---
+++ frameworks/opengl_wrapper/src/EGL/egl_blob_cache.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+namespace OHOS {
+class BlobCache {
+    public:
+    static BlobCache* get();
+    static void BlobCache::setBlobFunc(const void* key, EGLsizeiANDROID keySize, const void* value,
+                    EGLsizeiANDROID valueSize)
+    static EGLsizeiANDROID getBlobFunc(const void* key, EGLsizeiANDROID keySize, void* value,
+                               EGLsizeiANDROID valueSize);
+
+    void BlobCache::setBlob(const void* key, EGLsizeiANDROID keySize, const void* value,
+                    EGLsizeiANDROID valueSize)
+    EGLsizeiANDROID getBlob(const void* key, EGLsizeiANDROID keySize, void* value,
+                               EGLsizeiANDROID valueSize);
+    void initialize(EGLDisplay dpy);
+    private:
+    static BlobCache *blob_cache_;
+}
+}

af73b9ff9d3ad52a6eada203ac7cba30574ae253
DESKTOP-7S59O5K\Lenovo <test>
2024-03-18 09:43:42 +0800

    修改random 方式，参考skia做法，去除 canvasDrawOval

--- rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/testcase/draw_path_arc_to.cpp
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/testcase/draw_path_arc_to.cpp
@@ -5,7 +5,8 @@
 #include <native_drawing/drawing_rect.h>
 #include <time.h>
 #include "common/log_common.h"
-#include "common/util.h"
+//#include "common/util.h"
+#include "test_common.h"
 
 void DrawPathArcTo::OnTestFunction(OH_Drawing_Canvas* canvas)
 {
@@ -16,25 +17,25 @@ void DrawPathArcTo::OnTestFunction(OH_Drawing_Canvas* canvas)
     OH_Drawing_CanvasAttachPen(canvas, pen);
     
     OH_Drawing_Rect* rect;// = OH_Drawing_RectCreate(0, 0, 100, 100);
-    init_random_seed();
+    TestRend rand = TestRend(time(NULL));
     // 在画布上画path的形状
     OH_Drawing_Path* path = OH_Drawing_PathCreate();
-    float radius = next_uscalar1()*3;
-    float left = next_uscalar1()*300;
-    float top = next_uscalar1()*300;
+    float radius = rand.nextUScalar1()*3;
+    float left = rand.nextUScalar1()*300;
+    float top = rand.nextUScalar1()*300;
     float right = left+2*radius;
     float bottom = top+2*radius;
     rect = OH_Drawing_RectCreate(left,top,right,bottom);
-    OH_Drawing_PenSetWidth(pen, next_uscalar1()*5.0f);
+    OH_Drawing_PenSetWidth(pen, rand.nextUScalar1()*5.0f);
     OH_Drawing_CanvasAttachPen(canvas, pen);
     OH_Drawing_PathArcTo(path,left,top,right,bottom,0,0);
     //path.addOval 目前还没开放
-    OH_Drawing_CanvasDrawOval(canvas,rect);
+//    OH_Drawing_CanvasDrawOval(canvas,rect);  OH_Drawing_Path addOval 还未启用 先不测
     OH_Drawing_PathArcTo(path,left,top,right,bottom,360,0);
-    OH_Drawing_CanvasDrawOval(canvas,rect);
+//    OH_Drawing_CanvasDrawOval(canvas,rect);
     OH_Drawing_PathClose(path);
     OH_Drawing_CanvasDrawPath(canvas, path);
-//        DRAWING_LOGI("arcto rand value = %f\n",next_uscalar1());
+
     OH_Drawing_PathDestroy(path);
 
     OH_Drawing_PenDestroy(pen);
@@ -51,26 +52,26 @@ void DrawPathArcTo::OnTestPerformance(OH_Drawing_Canvas* canvas)
     OH_Drawing_CanvasAttachPen(canvas, pen);
     
     OH_Drawing_Rect* rect;
-    init_random_seed();
+    TestRend rand = TestRend(time(NULL));
     for (int i = 0; i < testCount_; i++) {
         // 在画布上画path的形状
         OH_Drawing_Path* path = OH_Drawing_PathCreate();
-        float radius = next_uscalar1()*3;
-        float left = next_uscalar1()*300;
-        float top = next_uscalar1()*300;
+        float radius = rand.nextUScalar1()*3;
+        float left = rand.nextUScalar1()*300;
+        float top = rand.nextUScalar1()*300;
         float right = left+2*radius;
         float bottom = top+2*radius;
         rect = OH_Drawing_RectCreate(left,top,right,bottom);
-        OH_Drawing_PenSetWidth(pen, next_uscalar1()*5.0f);
+        OH_Drawing_PenSetWidth(pen, rand.nextUScalar1()*5.0f);
         OH_Drawing_CanvasAttachPen(canvas, pen);
         OH_Drawing_PathArcTo(path,left,top,right,bottom,0,0);
         //path.addOval 目前还没开放
-        OH_Drawing_CanvasDrawOval(canvas,rect);
+//        OH_Drawing_CanvasDrawOval(canvas,rect); OH_Drawing_Path addOval 还未启用 先不测
         OH_Drawing_PathArcTo(path,left,top,right,bottom,360,0);
-        OH_Drawing_CanvasDrawOval(canvas,rect);
+//        OH_Drawing_CanvasDrawOval(canvas,rect);
         OH_Drawing_PathClose(path);
         OH_Drawing_CanvasDrawPath(canvas, path);
-//        DRAWING_LOGI("arcto rand value = %f\n",next_uscalar1());
+//        DRAWING_LOGI("arcto rand value = %{public}f\n",rand.nextUScalar1());
         OH_Drawing_PathDestroy(path);
     }
 

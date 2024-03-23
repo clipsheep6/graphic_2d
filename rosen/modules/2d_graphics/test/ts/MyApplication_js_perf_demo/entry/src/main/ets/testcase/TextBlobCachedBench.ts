6099f8bf4327b43b1c2a4f6b8d4e021b097d6c70
x30034819 <xieyijun3@huawei.com>
2024-03-23 21:17:31 +0800

    rename ts demo Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Signed-off-by: x30034819 <xieyijun3@huawei.com>

---
+++ rosen/modules/2d_graphics/test/ts/MyApplication_js_perf_demo/entry/src/main/ets/testcase/TextBlobCachedBench.ts
@@ -0,0 +1,37 @@
+
+import drawing from "@ohos.graphics.drawing";
+import common2D from "@ohos.graphics.common2D";
+import {TestBase} from '../pages/testbase';
+
+const TAG = '[DrawingTest]';
+
+export class TextBlobCachedBench extends TestBase {
+  public constructor() {
+    // 根据需求，如果与默认值不一样，请继承重写
+    super();
+    this.fileName_ = "drawtextblob";
+  }
+
+  public OnTestFunction(canvas: drawing.Canvas) {
+    //接口调用，功能测试.cpu/gpu调用接口一致
+    const brush = new drawing.Brush();
+    brush.setColor({alpha: 255, red: 255, green: 255, blue: 255});
+    const font = new drawing.Font();
+    font.setSize(10);
+    const textBlob = drawing.TextBlob.makeFromString("Keep your sentences short, but not overly so.", font, drawing.TextEncoding.TEXT_ENCODING_UTF8);
+    canvas.attachBrush(brush);
+    canvas.drawTextBlob(textBlob, 20, 20);
+    canvas.detachBrush();
+  }
+
+  // TextBlobCachedBench
+  public OnTestPerformance(canvas: drawing.Canvas) {
+    //接口重复调用，性能功耗测试 cpu/gpu调用接口一致
+    const font = new drawing.Font();
+    // font.setSize(10);
+    const textBlob = drawing.TextBlob.makeFromString("Keep your sentences short, but not overly so.", font, drawing.TextEncoding.TEXT_ENCODING_UTF8);
+    for (let index = 0; index < this.testCount_; index++) {
+      canvas.drawTextBlob(textBlob, 20, 20);
+    }
+  }
+}

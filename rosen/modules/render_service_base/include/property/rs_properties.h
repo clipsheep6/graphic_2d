17415a0c16a057bf0e6d742d4cb315d40f524de7
jwy888 <jiang_wen_yu@yeah.net>
2024-03-24 13:22:08 +0000

    test
    Signed-off-by: jwy888 <jiang_wen_yu@yeah.net>

--- rosen/modules/render_service_base/include/property/rs_properties.h
+++ rosen/modules/render_service_base/include/property/rs_properties.h
@@ -16,6 +16,9 @@
 #ifndef RENDER_SERVICE_CLIENT_CORE_PROPERTY_RS_PROPERTIES_H
 #define RENDER_SERVICE_CLIENT_CORE_PROPERTY_RS_PROPERTIES_H
 
+
+
+
 #include <bitset>
 #include <optional>
 #include <tuple>

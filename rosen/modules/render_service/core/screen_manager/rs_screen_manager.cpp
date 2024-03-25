be3b69be30fd4d9f94d6408799e8556f7f9e4168
lee <liwei576@huawei.com>
2024-03-25 17:04:32 +0800

    部件化整改
    Signed-off-by: lee <liwei576@huawei.com>

--- rosen/modules/render_service/core/screen_manager/rs_screen_manager.cpp
+++ rosen/modules/render_service/core/screen_manager/rs_screen_manager.cpp
@@ -1121,7 +1121,13 @@ ScreenScaleMode RSScreenManager::GetScaleMode(ScreenId id) const
         RS_LOGW("RSScreenManager::GetScaleMode: There is no screen for id %{public}" PRIu64 ".", id);
         return ScreenScaleMode::INVALID_MODE;
     }
-    return screens_.at(id)->GetScaleMode();
+    auto scaleModeDFX = static_cast<ScreenScaleMode>(
+        RSSystemProperties::GetVirtualScreenScaleModeDFX());
+    // Support mode can be configured for maintenance and testing before
+    // upper layer application adaptation
+    const auto& scaleMode = (scaleModeDFX == ScreenScaleMode::INVALID_MODE) ?
+        screens_.at(id)->GetScaleMode() : scaleModeDFX;
+    return scaleMode;
 }
 
 sptr<Surface> RSScreenManager::GetProducerSurface(ScreenId id) const
@@ -1214,6 +1220,16 @@ void RSScreenManager::ClearFpsDump(std::string& dumpString, std::string& arg)
     dumpString += "\n-- Clear fps records info of screens:\n";
     for (const auto &[id, screen] : screens_) {
         screen->ClearFpsDump(index, dumpString, arg);
+        index++;
+    }
+}
+
+void RSScreenManager::HitchsDump(std::string& dumpString, std::string& arg)
+{
+    int32_t index = 0;
+    dumpString += "\n-- The recently window hitchs records info of screens:\n";
+    for (const auto &[id, screen] : screens_) {
+        screen->HitchsDump(index, dumpString, arg);
         index++;
     }
 }
--- rosen/test/render_service/render_service/unittest/screen_manager/rs_screen_test.cpp
+++ rosen/test/render_service/render_service/unittest/screen_manager/rs_screen_test.cpp
@@ -16,7 +16,7 @@
 #include "gtest/gtest.h"
 #include "limit_number.h"
 #include "screen_manager/rs_screen.h"
-#include "../pipeline/mock/mock_hdi_device.h"
+#include "mock/mock_hdi_device.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -492,5 +492,48 @@ HWTEST_F(RSScreenTest, ClearFpsDumpTest_002, testing::ext::TestSize.Level1)
     std::string dumpString = "ClearFpsDumpTest";
     std::string arg = "ClearFpsDumpTest";
     rsScreen->ClearFpsDump(screenIndex, dumpString, arg);
+}
+
+/*
+ * @tc.name: PowerStatusDump_001
+ * @tc.desc: PowerStatusDump Test, virtual screen
+ * @tc.type: FUNC
+ * @tc.require: issueI78T3Z
+ */
+HWTEST_F(RSScreenTest, PowerStatusDump_001, testing::ext::TestSize.Level1)
+{
+    VirtualScreenConfigs config;
+    config.id = 1;
+    auto rsScreen = std::make_unique<impl::RSScreen>(config);
+    ASSERT_NE(rsScreen, nullptr);
+    uint32_t status = GraphicDispPowerStatus::GRAPHIC_POWER_STATUS_ON;
+    rsScreen->SetPowerStatus(status);
+    std::string dumpString = "";
+    rsScreen->PowerStatusDump(dumpString);
+    ASSERT_TRUE(dumpString=="powerstatus=INVALID_POWER_STATUS");
+}
+
+/*
+ * @tc.name: PowerStatusDump_002
+ * @tc.desc: PowerStatusDump Test, with mocked HDI device
+ * @tc.type: FUNC
+ * @tc.require: issueI78T3Z
+ */
+HWTEST_F(RSScreenTest, PowerStatusDump_002, testing::ext::TestSize.Level1)
+{
+    ScreenId screenId = mockScreenId_;
+    auto hdiOutput = HdiOutput::CreateHdiOutput(screenId);
+    auto rsScreen = std::make_unique<impl::RSScreen>(screenId, false, hdiOutput, nullptr);
+    rsScreen->hdiScreen_->device_ = hdiDeviceMock_;
+    // Set status to GRAPHIC_POWER_STATUS_ON_ADVANCED
+    rsScreen->SetPowerStatus(GraphicDispPowerStatus::GRAPHIC_POWER_STATUS_ON_ADVANCED);
+    std::string dumpString = "";
+    rsScreen->PowerStatusDump(dumpString);
+    ASSERT_TRUE(dumpString=="powerstatus=POWER_STATUS_ON_ADVANCED");
+    // Set status to GRAPHIC_POWER_STATUS_OFF_ADVANCED
+    rsScreen->SetPowerStatus(GraphicDispPowerStatus::GRAPHIC_POWER_STATUS_OFF_ADVANCED);
+    dumpString = "";
+    rsScreen->PowerStatusDump(dumpString);
+    ASSERT_TRUE(dumpString=="powerstatus=POWER_STATUS_OFF_ADVANCED");
 }
 } // namespace OHOS::Rosen

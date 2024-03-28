b2e495259972900a3295a117bfc47d48373bb6be
AnBetter <wangguan27@huawei.com>
2024-03-28 16:25:58 +0800

    格式化代码

--- rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/bench/hardstopgradientbench_scalenumcolors.cpp
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/bench/hardstopgradientbench_scalenumcolors.cpp
@@ -24,7 +24,8 @@ SkColor color_choices[kNumColorChoices] = {
 };
 SkColor colors[100];
 
-void HardStopGradientBench_ScaleNumColors::OnTestFunction(OH_Drawing_Canvas *canvas) {
+void HardStopGradientBench_ScaleNumColors::OnTestFunction(OH_Drawing_Canvas *canvas)
+{
     // 用例名: hardstop_scale_num_colors_setShader 测试 OH_Drawing_PenSetShaderEffect
     // 迁移基于skia HardStopGradientBench_ScaleNumColors->hardstopgradientbench_scalenumcolors
     DRAWING_LOGI("HardStopGradientBench_ScaleNumColors::OnTestFunction start");
@@ -53,33 +54,33 @@ void HardStopGradientBench_ScaleNumColors::OnTestFunction(OH_Drawing_Canvas *can
     }
 
     switch (effectType_) {
-    case kClamp:
-        DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kClamp");
-        effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
-                                                             OH_Drawing_TileMode::CLAMP);
-        break;
-    case kRepeat:
-        DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kRepeat");
+        case kClamp:
+            DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kClamp");
+            effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::CLAMP);
+            break;
+        case kRepeat:
+            DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kRepeat");
 
-        effect = OH_Drawing_ShaderEffectCreateRadialGradient(centerPt, kSize * 0.5, colors, positions, fColorCount_,
-                                                             OH_Drawing_TileMode::REPEAT);
-        break;
-    case kMirror:
-        DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kMirror");
-        effect = OH_Drawing_ShaderEffectCreateSweepGradient(centerPt, colors, positions, fColorCount_,
-                                                            OH_Drawing_TileMode::DECAL);
-        break;
-    default:
-        effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
-                                                             OH_Drawing_TileMode::CLAMP);
-        break;
+            effect = OH_Drawing_ShaderEffectCreateRadialGradient(centerPt, kSize * 0.5, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::REPEAT);
+            break;
+        case kMirror:
+            DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kMirror");
+            effect = OH_Drawing_ShaderEffectCreateSweepGradient(centerPt, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::DECAL);
+            break;
+        default:
+            effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::CLAMP);
+            break;
     }
     OH_Drawing_PenSetShaderEffect(pen, effect);
     OH_Drawing_CanvasAttachPen(canvas, pen);
     OH_Drawing_Rect *rect = OH_Drawing_RectCreate(0, 0, kSize, kSize);
     for (int i = 0; i < testCount_; i++) {
-        //源代码canvas->drawPaint(fPaint),鸿蒙中没有可测的接口.这里用画圆型代替.后续鸿蒙接口更新再加上.对于其他代码均是原逻辑.
-            // OH_Drawing_CanvasDrawOval(canvas, rect);
+        // 源代码canvas->drawPaint(fPaint),鸿蒙中没有可测的接口.这里用画圆型代替.后续鸿蒙接口更新再加上.对于其他代码均是原逻辑.
+        // OH_Drawing_CanvasDrawOval(canvas, rect);
     }
     OH_Drawing_ShaderEffectDestroy(effect);
     OH_Drawing_RectDestroy(rect);
@@ -89,7 +90,8 @@ void HardStopGradientBench_ScaleNumColors::OnTestFunction(OH_Drawing_Canvas *can
     DRAWING_LOGI("HardStopGradientBench_ScaleNumColors::OnTestFunction end");
 }
 
-void HardStopGradientBench_ScaleNumColors::OnTestPerformance(OH_Drawing_Canvas *canvas) {
+void HardStopGradientBench_ScaleNumColors::OnTestPerformance(OH_Drawing_Canvas *canvas)
+{
     // 用例名: hardstop_scale_num_colors_setShader 测试 OH_Drawing_PenSetShaderEffect
     // 迁移基于skia HardStopGradientBench_ScaleNumColors->hardstopgradientbench_scalenumcolors
     DRAWING_LOGI("HardStopGradientBench_ScaleNumColors::OnTestPerformance start");
@@ -99,7 +101,7 @@ void HardStopGradientBench_ScaleNumColors::OnTestPerformance(OH_Drawing_Canvas *
     OH_Drawing_PenSetWidth(pen, 10);
     OH_Drawing_PenSetAntiAlias(pen, true);
     OH_Drawing_CanvasAttachPen(canvas, pen);
-    
+
     OH_Drawing_ShaderEffect *effect;
     OH_Drawing_Point *startPt = OH_Drawing_PointCreate(0, kSize / 2);
     OH_Drawing_Point *endPt = OH_Drawing_PointCreate(kSize - 1, kSize / 2);
@@ -119,34 +121,34 @@ void HardStopGradientBench_ScaleNumColors::OnTestPerformance(OH_Drawing_Canvas *
     }
 
     switch (effectType_) {
-    case kClamp:
-        DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kClamp");
-        effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
-                                                             OH_Drawing_TileMode::CLAMP);
-        break;
-    case kRepeat:
-        DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kRepeat");
+        case kClamp:
+            DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kClamp");
+            effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::CLAMP);
+            break;
+        case kRepeat:
+            DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kRepeat");
 
-        effect = OH_Drawing_ShaderEffectCreateRadialGradient(centerPt, kSize * 0.5, colors, positions, fColorCount_,
-                                                             OH_Drawing_TileMode::REPEAT);
-        break;
-    case kMirror:
-        DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kMirror");
-        effect = OH_Drawing_ShaderEffectCreateSweepGradient(centerPt, colors, positions, fColorCount_,
-                                                            OH_Drawing_TileMode::DECAL);
-        break;
-    default:
-        effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
-                                                             OH_Drawing_TileMode::CLAMP);
-        break;
+            effect = OH_Drawing_ShaderEffectCreateRadialGradient(centerPt, kSize * 0.5, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::REPEAT);
+            break;
+        case kMirror:
+            DRAWING_LOGI("HardStopGradientBench_ScaleNumColors:kMirror");
+            effect = OH_Drawing_ShaderEffectCreateSweepGradient(centerPt, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::DECAL);
+            break;
+        default:
+            effect = OH_Drawing_ShaderEffectCreateLinearGradient(startPt, endPt, colors, positions, fColorCount_,
+                OH_Drawing_TileMode::CLAMP);
+            break;
     }
-    
+
     OH_Drawing_PenSetShaderEffect(pen, effect);
     OH_Drawing_CanvasAttachPen(canvas, pen);
     OH_Drawing_Rect *rect = OH_Drawing_RectCreate(0, 0, kSize, kSize);
     for (int i = 0; i < testCount_; i++) {
-        //源代码canvas->drawPaint(fPaint),鸿蒙中没有可测的接口.这里用画圆型代替.后续鸿蒙接口更新再加上.对于其他代码均是原逻辑.
-            // OH_Drawing_CanvasDrawOval(canvas, rect);
+        // 源代码canvas->drawPaint(fPaint),鸿蒙中没有可测的接口.这里用画圆型代替.后续鸿蒙接口更新再加上.对于其他代码均是原逻辑.
+        // OH_Drawing_CanvasDrawOval(canvas, rect);
     }
     OH_Drawing_ShaderEffectDestroy(effect);
     OH_Drawing_RectDestroy(rect);

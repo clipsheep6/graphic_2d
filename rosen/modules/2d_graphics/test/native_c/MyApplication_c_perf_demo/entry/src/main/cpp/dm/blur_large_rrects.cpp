b9876177c231148c78f361609fc8bc194a44065e
AnBetter <wangguan27@huawei.com>
2024-03-29 10:41:01 +0800

    blur_large_rrects

---
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/dm/blur_large_rrects.cpp
@@ -0,0 +1,67 @@
+#include "blur_large_rrects.h"
+#include <native_drawing/drawing_color.h>
+#include <native_drawing/drawing_brush.h>
+#include <native_drawing/drawing_matrix.h>
+#include <native_drawing/drawing_path.h>
+#include <native_drawing/drawing_pen.h>
+#include <native_drawing/drawing_mask_filter.h>
+#include <native_drawing/drawing_filter.h>
+#include <native_drawing/drawing_round_rect.h>
+#include <native_drawing/drawing_round_rect.h>
+#include "test_common.h"
+#include "common/log_common.h"
+
+BlurLargeRrects::BlurLargeRrects() {
+    bitmapWidth_ = 300;
+    bitmapHeight_ = 300;
+    fileName_ = "blurlargerrects";
+}
+
+void BlurLargeRrects::OnTestFunction(OH_Drawing_Canvas* canvas)
+{
+    // 创建一个pen对象
+    OH_Drawing_Pen *pen = OH_Drawing_PenCreate();
+    OH_Drawing_PenSetAntiAlias(pen, true);
+    int width = 3;
+    OH_Drawing_PenSetWidth(pen, width);
+    OH_Drawing_CanvasAttachPen(canvas, pen);
+    // 创建一个滤波器对象
+    OH_Drawing_Filter* filter = OH_Drawing_FilterCreate();
+    // 创建一个模板滤波器
+    float sigma = 20.f;
+    OH_Drawing_MaskFilter *maskFilter = OH_Drawing_MaskFilterCreateBlur(OH_Drawing_BlurType::NORMAL, sigma, true);
+    // 为滤波器设置模板
+    OH_Drawing_FilterSetMaskFilter(filter, maskFilter);
+    // 设置画笔滤波器
+    OH_Drawing_PenSetFilter(pen, filter);
+    // 创建矩形、圆角矩形
+    float left = 5.f;
+    float top = -20000.f;
+    float right = 240.f;
+    float bottom = 25.f;
+    OH_Drawing_Rect *rect = OH_Drawing_RectCreate(left, top, right, bottom);
+    float xRad = 40.f, yRad = 40.f;
+    OH_Drawing_RoundRect *rrect = OH_Drawing_RoundRectCreate(rect, xRad, yRad);
+
+    int MaxCount = 4;
+    for (int i = 0; i < MaxCount; ++i) {
+    //设置画笔颜色
+    int Bitwise = 1, Compare = 2;
+    float num = 1.f, number = 0.f, number2 = 1.0f;
+    OH_Drawing_PenSetColor(pen, OH_Drawing_ColorSetArgb((i & Bitwise) ? num : number, (i & Bitwise) ? num : number,
+                                                        (i < Compare) ? num : number, num));
+    // 上画布
+    OH_Drawing_CanvasDrawOval(canvas, rect);
+    // 设置画布旋转角度
+    float degrees = 90.f, px = 150.f, py = 150.f;
+    OH_Drawing_CanvasRotate(canvas, degrees, px, py);
+    }
+
+    // 释放内存
+    OH_Drawing_CanvasDetachPen(canvas);
+    OH_Drawing_MaskFilterDestroy(maskFilter);
+    OH_Drawing_FilterDestroy(filter);
+    OH_Drawing_RectDestroy(rect);
+    OH_Drawing_PenDestroy(pen);
+    pen = nullptr;
+}
--- rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/testcasefactory.cpp
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/testcasefactory.cpp
@@ -40,6 +40,7 @@
 #include "bench/clear_bench.h"
 #include "bench/big_path_bench.h"
 #include "dm/aa_rect_modes.h"
+#include "dm/blur_large_rrects.h"
 
 namespace {
     std::unordered_map<std::string, std::function<std::shared_ptr<TestBase>()>> FunctionalCpuMap =
@@ -122,6 +123,7 @@ namespace {
             
             //DM
             {"aarectmodes", []() -> std::shared_ptr<TestBase> { return std::make_shared<AARectModes>(); }},
+            {"blurlargerrects", []() -> std::shared_ptr<TestBase> { return std::make_shared<BlurLargeRrects>(); }},
             
 
     };

dcd5ab278ff2fa5dcaab7b2547eb93bac5f12a37
ZhangPeng <zhangpeng280@huawei.com>
2024-04-02 08:57:27 +0000

    !305 修复TDD和SDK编译 Merge pull request !305 from ZhangPeng/PR-fix-tdd-and-sdk-compile
    Change-Id: I347c640956a72fb8ac1693d605e86eeb3131b5e7

--- rosen/modules/2d_graphics/include/image/gpu_context.h
+++ rosen/modules/2d_graphics/include/image/gpu_context.h
@@ -15,11 +15,14 @@
 
 #ifndef GPU_CONTEXT_H
 #define GPU_CONTEXT_H
+#include <functional>
 #include <set>
-#include "impl_interface/gpu_context_impl.h"
-#include "utils/drawing_macros.h"
-#include "utils/data.h"
+
 #include "trace_memory_dump.h"
+
+#include "impl_interface/gpu_context_impl.h"
+#include "utils/data.h"
+#include "utils/drawing_macros.h"
 
 typedef void* EGLContext;
 namespace OHOS {

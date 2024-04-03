19f1ded8e11bb61e0187ac78d1f5f3b22c2dc198
wangxutao <wangxutao5@huawei.com>
2024-04-03 15:18:36 +0800

    RS persistcache mutex fix
    Signed-off-by: wangxutao <wangxutao5@huawei.com>

--- rosen/modules/2d_graphics/src/render_context/shader_cache.h
+++ rosen/modules/2d_graphics/src/render_context/shader_cache.h
@@ -28,6 +28,22 @@ class ShaderCache : public Drawing::GPUContextOptions::PersistentCache {
 public:
     static ShaderCache& Instance();
 
+    struct OptionalLockGuard {
+        explicit OptionalLockGuard(std::mutex& m): mtx_(m), status(m.try_lock()) {}
+
+        ~OptionalLockGuard() {
+            if (status_) {
+                mtx.unlock();
+            }
+        }
+
+        OptionalLockGuard(const OptionalLockGuard&) = delete;
+        OptionalLockGuard& operator=(const OptionalLockGuard&) = delete;
+
+        std::mutex& mtx_;
+        bool status = false;
+    };
+
     virtual void InitShaderCache(const char *identity, const size_t size, bool isUni);
     virtual void InitShaderCache()
     {

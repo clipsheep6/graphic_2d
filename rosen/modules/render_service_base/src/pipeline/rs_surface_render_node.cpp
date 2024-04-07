0e7776f2d79d1d8447c233005e81101122b30224
Zhang Peng <zhangpeng280@huawei.com>
2024-04-07 09:45:46 +0800

    fix TDD & SDK compile error
    Signed-off-by: Zhang Peng <zhangpeng280@huawei.com>
    Change-Id: Iccd437f6f8b5a0265927b491d863776a62efdffc

--- rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
@@ -2294,7 +2294,6 @@ void RSSurfaceRenderNode::SetUifirstChildrenDirtyRectParam(RectI rect)
     } else {
         RS_LOGE("RSSurfaceRenderNode::SetUifirstFlag stagingSurfaceParams is null");
     }
-
 }
 
 void RSSurfaceRenderNode::SetUifirstNodeEnableParam(bool b)

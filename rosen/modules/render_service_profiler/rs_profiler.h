4c3dde7e8c4446843da3f6307a4ee0766fe8be7a
vyalkovdmitry <vyalkov.dmitry@huawei-partners.com>
2024-04-09 11:20:14 +0000

    update rosen/modules/render_service_profiler/rs_profiler.h.
    Signed-off-by: vyalkovdmitry <vyalkov.dmitry@huawei-partners.com>

--- rosen/modules/render_service_profiler/rs_profiler.h
+++ rosen/modules/render_service_profiler/rs_profiler.h
@@ -17,12 +17,12 @@
 #define RENDER_SERVICE_PROFILER_H
 
 #include <map>
-#include <nlohmann/json_fwd.hpp>
 #include <string>
-
 #include "common/rs_vector4.h"
 
 #ifdef RS_PROFILER_ENABLED
+#include <nlohmann/json_fwd.hpp>
+
 #define RS_PROFILER_INIT(renderSevice) RSProfiler::Init(renderSevice)
 #define RS_PROFILER_ON_FRAME_BEGIN() RSProfiler::OnFrameBegin()
 #define RS_PROFILER_ON_FRAME_END() RSProfiler::OnFrameEnd()

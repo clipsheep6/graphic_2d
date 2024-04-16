96bfbfcfd6eb077c938785e1d268051b62a5f3c0
sunqizhen <sunqizhen2@huawei.com>
2024-04-16 17:18:35 +0800

    fix tdd crash and failure
    Signed-off-by: sunqizhen <sunqizhen2@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.h
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.h
@@ -381,10 +381,6 @@ private:
         const Occlusion::Region& visibleRegion);
     void PrintCurrentStatus();
     void WaitUntilUploadTextureTaskFinishedForGL();
-#ifdef RES_SCHED_ENABLE
-    void SubScribeSystemAbility();
-    sptr<VSyncSystemAbilityListener> saStatusChangeListener_ = nullptr;
-#endif
 
     bool DoDirectComposition(std::shared_ptr<RSBaseRenderNode> rootNode, bool waitForRT);
 
@@ -566,6 +562,12 @@ private:
 #ifdef RS_PROFILER_ENABLED
     friend class RSProfiler;
 #endif
+
+#ifdef RES_SCHED_ENABLE
+    void SubScribeSystemAbility();
+    sptr<VSyncSystemAbilityListener> saStatusChangeListener_ = nullptr;
+#endif
+
     pid_t exitedPid_ = -1;
     std::set<pid_t> exitedPidSet_;
     RSDrawFrame drawFrame_;

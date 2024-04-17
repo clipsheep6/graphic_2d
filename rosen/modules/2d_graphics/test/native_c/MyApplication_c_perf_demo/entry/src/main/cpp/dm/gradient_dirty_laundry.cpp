72c7f1fa8b81f6aac6ae5bffd95d4ad118066453
AnBetter <wangguan27@huawei.com>
2024-04-17 17:39:03 +0800

    linan_ceshi

--- rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/dm/gradient_dirty_laundry.cpp
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/dm/gradient_dirty_laundry.cpp
@@ -15,10 +15,7 @@
 #include "test_common.h"
 
 
-enum {
-    WIDTH = 640,
-    HEIGHT = 615
-};
+enum { WIDTH = 640, HEIGHT = 615 };
 
 struct GradData {
     int fCount = 40;
@@ -26,30 +23,30 @@ struct GradData {
     const float *fPos;
 };
 
-static const uint32_t gColors[] = {
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
-    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
+
+constexpr int kNumColorChoices = 50;
+
+SkColor gColors[kNumColorChoices] = {
+    0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF,
+    0xFF000000, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF,
+    0xFFFFFFFF, 0xFF000000, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000, 0xFFFF0000, 0xFF00FF00,
+    0xFF0000FF, 0xFFFFFFFF, 0xFF000000, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000, 0xFFFF0000,
+    0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000, 0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000,
     0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000, // 10 lines, 50 colors
 };
-// static const float_t fPosa[] = {4.00f, 4.00f};
-// constexpr GradData gGradData[] = {
+SkColor colors[100];
+//static const float_t fPosa[] = {4.00f, 4.00f};
+//constexpr GradData gGradData[] = {
 //    {40, gColors, fPosa},
-// };
+//};
 constexpr SkScalar gPos[] = {0.25f, 0.75f};
-// constexpr SkScalar gPos[] = {0.00f, 0.00f};
-// static const uint32_t gShallowColors[] = {0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000};
-// constexpr SkScalar gPos[] = {SK_Scalar1 * 999 / 2000, SK_Scalar1 * 999 / 2000};
+//constexpr SkScalar gPos[] = {0.00f, 0.00f};
+//static const uint32_t gShallowColors[] = {0xFFFF0000, 0xFF00FF00, 0xFF0000FF, 0xFFFFFFFF, 0xFF000000};
+//constexpr SkScalar gPos[] = {SK_Scalar1 * 999 / 2000, SK_Scalar1 * 999 / 2000};
 constexpr GradData gGradData[] = {
     {40, gColors, gPos},
-    //    {2, gShallowColors, gPos},
-    //      { 2, gColors, gPos },
+//    {2, gShallowColors, gPos},
+//      { 2, gColors, gPos },
 };
 GradData data = gGradData[0];
 // 圆心和圆半径坐标
@@ -58,117 +55,151 @@ OH_Drawing_Point2D pt[] = {SkScalarAve(pts[0].x, pts[1].x), SkScalarAve(pts[0].y
 OH_Drawing_Point2D Point = pt[0];
 float x = Point.x;
 float y = Point.y;
-OH_Drawing_Point *centerPt = OH_Drawing_PointCreate(Point.x, Point.y);
+ OH_Drawing_Point *centerPt = OH_Drawing_PointCreate(Point.x, Point.y);
 //  渐变的起点和终点坐标
 OH_Drawing_Point *firstPoint = OH_Drawing_PointCreate(0, 0);
 OH_Drawing_Point *secondPoint = OH_Drawing_PointCreate(SkIntToScalar(100), SkIntToScalar(100));
 int fCount = 40;
+
+
+
+
 // 定义三种着色器
 OH_Drawing_ShaderEffect *MakeLinear(const OH_Drawing_Point *firstPoint, const OH_Drawing_Point *secondPoint,
-    const GradData &data, OH_Drawing_TileMode tileMode)
-{
+                                    const GradData &data, OH_Drawing_TileMode tileMode) {
+    for (int i = 0; i < fCount; i++) {
+        colors[i] = gColors[i % kNumColorChoices];
+    }
+    int ArraySize = 100;
+    float positions[ArraySize];
+    // 初始化数组的前两个元素为0.0f
+    positions[0] = 0.0f;
+    positions[1] = 0.0f;
+    // 用等间距的值填充数组的其余部分
+    for (int i = 2; i < fCount; ++i) {
+        positions[i] = static_cast<float>(i) / (fCount - 1.0f);
+    }
+
+
     DRAWING_LOGI("GradientsGM MakeLinear");
-    OH_Drawing_ShaderEffect *MakeLinear = OH_Drawing_ShaderEffectCreateLinearGradient(firstPoint, secondPoint,
-        data.fColors, data.fPos, fCount, OH_Drawing_TileMode::CLAMP);
-    DRAWING_LOGI("GradientsGM firstPoint = %{public}p,secondPoint = %{public}p,fColors = "
-                 "%{public}u,fPos=%{public}f,fCount=%{public}d",
-        firstPoint, secondPoint, data.fColors, data.fPos, fCount);
-    DRAWING_LOGI("GradientsGM MakeLinear = %{public}p", MakeLinear);
+//    OH_Drawing_ShaderEffect *MakeLinear = OH_Drawing_ShaderEffectCreateLinearGradient(
+//        firstPoint, secondPoint, data.fColors, data.fPos, fCount, OH_Drawing_TileMode::CLAMP);
+    OH_Drawing_ShaderEffect *MakeLinear = OH_Drawing_ShaderEffectCreateLinearGradient(
+        firstPoint, secondPoint, colors, positions, fCount, OH_Drawing_TileMode::CLAMP);
+//    DRAWING_LOGI("GradientsGM firstPoint = %{public}p,secondPoint = %{public}p,fColors = %{public}u,fPos=%{public}f,fCount=%{public}d", firstPoint,secondPoint,data.fColors,data.fPos,fCount);
+//    DRAWING_LOGI("GradientsGM MakeLinear = %{public}p", MakeLinear);
     return MakeLinear;
 }
 OH_Drawing_ShaderEffect *MakeRadial(const OH_Drawing_Point *firstPoint, const OH_Drawing_Point *secondPoint,
-    const GradData &data, OH_Drawing_TileMode tileMode)
-{
+                                    const GradData &data, OH_Drawing_TileMode tileMode) {
     DRAWING_LOGI("GradientsGM MakeRadial");
-    OH_Drawing_ShaderEffect *MakeRadial = OH_Drawing_ShaderEffectCreateRadialGradient(centerPt, pt[0].x, data.fColors,
-        data.fPos, fCount, OH_Drawing_TileMode::REPEAT);
-    DRAWING_LOGI(
-        "GradientsGM centerPt = %{public}p,pt[0].x = %{public}f,fColors = %{public}u,fPos=%{public}f,fCount=%{public}d",
-        centerPt, pt[0].x, data.fColors, data.fPos, fCount);
-    DRAWING_LOGI("GradientsGM MakeRadial = %{public}p", MakeRadial);
+    for (int i = 0; i < fCount; i++) {
+        colors[i] = gColors[i % kNumColorChoices];
+    }
+    int ArraySize = 100;
+    float positions[ArraySize];
+    // 初始化数组的前两个元素为0.0f
+    positions[0] = 0.0f;
+    positions[1] = 0.0f;
+    // 用等间距的值填充数组的其余部分
+    for (int i = 2; i < fCount; ++i) {
+        positions[i] = static_cast<float>(i) / (fCount - 1.0f);
+    }
+    OH_Drawing_ShaderEffect *MakeRadial = OH_Drawing_ShaderEffectCreateRadialGradient(
+        centerPt, pt[0].x, colors, positions, fCount, OH_Drawing_TileMode::REPEAT);
+//    DRAWING_LOGI("GradientsGM centerPt = %{public}p,pt[0].x = %{public}f,fColors = %{public}u,fPos=%{public}f,fCount=%{public}d", centerPt, pt[0].x, data.fColors, data.fPos, fCount);
+//    DRAWING_LOGI("GradientsGM MakeRadial = %{public}p", MakeRadial);
     return MakeRadial;
 }
 OH_Drawing_ShaderEffect *MakeSweep(const OH_Drawing_Point *firstPoint, const OH_Drawing_Point *secondPoint,
-    const GradData &data, OH_Drawing_TileMode tileMode)
-{
+                                   const GradData &data, OH_Drawing_TileMode tileMode) {
     DRAWING_LOGI("GradientsGM MakeSweep");
-    OH_Drawing_ShaderEffect *MakeSweep = OH_Drawing_ShaderEffectCreateSweepGradient(centerPt, data.fColors, data.fPos,
-        fCount, OH_Drawing_TileMode::DECAL);
-    DRAWING_LOGI("GradientsGM centerPt = %{public}p, fColors = %{public}u,fPos=%{public}f,fCount=%{public}d", centerPt,
-        data.fColors, data.fPos, fCount);
-    DRAWING_LOGI("GradientsGM MakeSweep = %{public}p", MakeSweep);
+    for (int i = 0; i < fCount; i++) {
+        colors[i] = gColors[i % kNumColorChoices];
+    }
+    int ArraySize = 100;
+    float positions[ArraySize];
+    // 初始化数组的前两个元素为0.0f
+    positions[0] = 0.0f;
+    positions[1] = 0.0f;
+    // 用等间距的值填充数组的其余部分
+    for (int i = 2; i < fCount; ++i) {
+        positions[i] = static_cast<float>(i) / (fCount - 1.0f);
+    }
+    OH_Drawing_ShaderEffect *MakeSweep = OH_Drawing_ShaderEffectCreateSweepGradient(
+        centerPt, colors, positions, fCount, OH_Drawing_TileMode::DECAL);
+//    DRAWING_LOGI("GradientsGM centerPt = %{public}p, fColors = %{public}u,fPos=%{public}f,fCount=%{public}d", centerPt, data.fColors, data.fPos, fCount);
+//    DRAWING_LOGI("GradientsGM MakeSweep = %{public}p", MakeSweep);
     return MakeSweep;
 }
 
 typedef OH_Drawing_ShaderEffect *(*GradMaker)(const OH_Drawing_Point *firstPoint, const OH_Drawing_Point *secondPoint,
-    const GradData &, OH_Drawing_TileMode);
+                                              const GradData &, OH_Drawing_TileMode);
 constexpr GradMaker gGradMakers[] = {
     MakeLinear,
     MakeRadial,
     MakeSweep,
 };
 
-GradientsGM::GradientsGM()
-{
+GradientsGM::GradientsGM() {
     // skia dm file gm/gradient_dirty_laundry.cpp
     bitmapWidth_ = WIDTH;
     bitmapHeight_ = HEIGHT;
     fileName_ = "gradient_dirty_laundry";
 }
 
-void GradientsGM::OnTestFunction(OH_Drawing_Canvas *canvas)
-{
+void GradientsGM::OnTestFunction(OH_Drawing_Canvas *canvas) {
     DRAWING_LOGI("GradientsGM::OnTestFunction start");
-    DRAWING_LOGI("GradientsGM centerPt = %{public}p,pt[0].x = %{public}f,firstPoint = %{public}p,secondPoint = "
-                 "%{public}p,fColors = %{public}u,fPos=%{public}f,fCount=%{public}d",
-        centerPt, pt[0].x, firstPoint, secondPoint, data.fColors, data.fPos, data.fCount);
+//    DRAWING_LOGI("GradientsGM centerPt = %{public}p,pt[0].x = %{public}f,firstPoint = %{public}p,secondPoint = %{public}p,fColors = %{public}u,fPos=%{public}f,fCount=%{public}d", centerPt, pt[0].x,
+//                 firstPoint, secondPoint, data.fColors, data.fPos, data.fCount);
     // 使用指定颜色设置清空画布底色
     OH_Drawing_CanvasClear(canvas, 0xFFDDDDDD);
-    // 创建一个画刷brush对象设置抗锯齿属性
-    OH_Drawing_Brush *brush = OH_Drawing_BrushCreate();
-    OH_Drawing_BrushSetAntiAlias(brush, true);
-    //        OH_Drawing_CanvasAttachBrush(canvas, brush);
-    // //     创建一个画刷pen对象设置抗锯齿属性
-    //        OH_Drawing_Pen *pen = OH_Drawing_PenCreate();
-    //        OH_Drawing_PenSetWidth(pen, 2);
-    //        OH_Drawing_PenSetAntiAlias(pen, true);
+        // 创建一个画刷brush对象设置抗锯齿属性
+        OH_Drawing_Brush *brush = OH_Drawing_BrushCreate();
+        OH_Drawing_BrushSetAntiAlias(brush, true);
+//        OH_Drawing_CanvasAttachBrush(canvas, brush);
+        ////     创建一个画刷pen对象设置抗锯齿属性
+        //        OH_Drawing_Pen *pen = OH_Drawing_PenCreate();
+        //        OH_Drawing_PenSetWidth(pen, 2);
+        //        OH_Drawing_PenSetAntiAlias(pen, true);
+    
+        // 创建一个矩形对象
+        OH_Drawing_Rect *rectAngLe = OH_Drawing_RectCreate(0, 0, SkIntToScalar(100), SkIntToScalar(100));
+        // 平移画布
+        OH_Drawing_CanvasTranslate(canvas, SkIntToScalar(20), SkIntToScalar(20));
 
-    // 创建一个矩形对象
-    OH_Drawing_Rect *rectAngLe = OH_Drawing_RectCreate(0, 0, SkIntToScalar(100), SkIntToScalar(100));
-    // 平移画布
-    OH_Drawing_CanvasTranslate(canvas, SkIntToScalar(20), SkIntToScalar(20));
+        for (size_t i = 0; i < SK_ARRAY_COUNT(gGradData); i++) {
+//            DRAWING_LOGI("GradientsGM::OnTestFunction i = %{public}d", i);
+            OH_Drawing_CanvasSave(canvas);
+            for (size_t j = 0; j < SK_ARRAY_COUNT(gGradMakers); j++) {
+//                DRAWING_LOGI("GradientsGM::OnTestFunction j = %{public}d", j);
+                OH_Drawing_BrushSetShaderEffect(
+                    brush, gGradMakers[j](firstPoint, secondPoint, gGradData[i], OH_Drawing_TileMode::CLAMP));
+                // OH_Drawing_PenSetShaderEffect(pen, gGradMakers[j](firstPoint, secondPoint, gGradData[i],
+                // OH_Drawing_TileMode::CLAMP));
 
-    for (size_t i = 0; i < SK_ARRAY_COUNT(gGradData); i++) {
-        DRAWING_LOGI("GradientsGM::OnTestFunction i = %{public}d", i);
-        OH_Drawing_CanvasSave(canvas);
-        for (size_t j = 0; j < SK_ARRAY_COUNT(gGradMakers); j++) {
-            DRAWING_LOGI("GradientsGM::OnTestFunction j = %{public}d", j);
-            OH_Drawing_BrushSetShaderEffect(brush,
-                gGradMakers[j](firstPoint, secondPoint, gGradData[i], OH_Drawing_TileMode::CLAMP));
-            // OH_Drawing_PenSetShaderEffect(pen, gGradMakers[j](firstPoint, secondPoint, gGradData[i],
-            // OH_Drawing_TileMode::CLAMP));
+                OH_Drawing_CanvasAttachBrush(canvas, brush);
+                // OH_Drawing_CanvasAttachPen(canvas, pen);
+                OH_Drawing_CanvasDrawRect(canvas, rectAngLe);
 
-            OH_Drawing_CanvasAttachBrush(canvas, brush);
-            // OH_Drawing_CanvasAttachPen(canvas, pen);
-            OH_Drawing_CanvasDrawRect(canvas, rectAngLe);
-
-            OH_Drawing_CanvasTranslate(canvas, 0, SkIntToScalar(120));
+                OH_Drawing_CanvasTranslate(canvas, 0, SkIntToScalar(120));
 
 
-            DRAWING_LOGI("GradientsGM::OnTestFunction gGradMakers_j = %{public}p",
-                gGradMakers[j](firstPoint, secondPoint, gGradData[i], OH_Drawing_TileMode::CLAMP));
-        }
-        OH_Drawing_CanvasRestore(canvas);
-        OH_Drawing_CanvasTranslate(canvas, SkIntToScalar(120), 0);
+//                DRAWING_LOGI("GradientsGM::OnTestFunction gGradMakers_j = %{public}p",
+//                             gGradMakers[j](firstPoint, secondPoint, gGradData[i], OH_Drawing_TileMode::CLAMP));
+            }
+            OH_Drawing_CanvasRestore(canvas);
+            OH_Drawing_CanvasTranslate(canvas, SkIntToScalar(120), 0);
+        
     }
     DRAWING_LOGI("GradientsGM::OnTestFunction end");
-
+    
     OH_Drawing_CanvasDestroy(canvas);
     OH_Drawing_RectDestroy(rectAngLe);
     OH_Drawing_CanvasDetachBrush(canvas);
     OH_Drawing_BrushDestroy(brush);
     brush = nullptr;
-    //        OH_Drawing_CanvasDetachPen(canvas);
-    //        OH_Drawing_PenDestroy(pen);
-    //        pen = nullptr;
+//        OH_Drawing_CanvasDetachPen(canvas);
+//        OH_Drawing_PenDestroy(pen);
+//        pen = nullptr;
 }
--- rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/dm/gradient_dirty_laundry.h
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/dm/gradient_dirty_laundry.h
@@ -17,7 +17,45 @@ typedef float SkScalar;
 #define SkIntToScalar(x) static_cast<SkScalar>(x)
 #define SkScalarAve(a, b) (((a) + (b)) * SK_ScalarHalf)
 
-/* * @return the number of entries in an array (not a pointer)
+typedef uint32_t SkColor;
+typedef unsigned U8CPU;
+
+static constexpr inline SkColor SkColorSetARGB(U8CPU a, U8CPU r, U8CPU g, U8CPU b) {
+    if (a <= 255 && r <= 255 && g <= 255 && b <= 255) {
+        return (a << 24) | (r << 16) | (g << 8) | (b << 0);
+    }
+    return (a << 24) | (r << 16) | (g << 8) | (b << 0);
+}
+
+/* * Returns color value from 8-bit component values. Asserts if SK_DEBUG is defined
+    if a, r, g, or b exceed 255. Since color is unpremultiplied, a may be smaller
+    than the largest of r, g, and b.
+
+    @param a  amount of alpha, from fully transparent (0) to fully opaque (255)
+    @param r  amount of red, from no red (0) to full red (255)
+    @param g  amount of green, from no green (0) to full green (255)
+    @param b  amount of blue, from no blue (0) to full blue (255)
+    @return   color and alpha, unpremultiplied
+*/
+/* * Represents fully opaque red.
+ */
+constexpr SkColor SK_ColorRED = SkColorSetARGB(0xFF, 0xFF, 0x00, 0x00);
+
+/* * Represents fully opaque green. HTML lime is equivalent.
+    Note that HTML green is equivalent to 0xFF008000.
+*/
+constexpr SkColor SK_ColorGREEN = SkColorSetARGB(0xFF, 0x00, 0xFF, 0x00);
+
+/* * Represents fully opaque blue.
+ */
+constexpr SkColor SK_ColorBLUE = SkColorSetARGB(0xFF, 0x00, 0x00, 0xFF);
+
+/* * Represents fully opaque yellow.
+ */
+constexpr SkColor SK_ColorYELLOW = SkColorSetARGB(0xFF, 0xFF, 0xFF, 0x00);
+
+
+/** @return the number of entries in an array (not a pointer)
  */
 template <typename T, size_t N> char (&SkArrayCountHelper(T (&array)[N]))[N];
 #define SK_ARRAY_COUNT(array) (sizeof(SkArrayCountHelper(array)))
@@ -28,6 +66,8 @@ public:
     ~GradientsGM() = default;
 
 protected:
+    
+
     void OnTestFunction(OH_Drawing_Canvas *canvas) override;
 };
 #endif // GRADIENTSGM_H

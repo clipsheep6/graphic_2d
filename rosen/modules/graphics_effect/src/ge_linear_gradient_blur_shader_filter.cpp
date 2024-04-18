276ac2a5cc8001a6f6f745a3a422457b53088ec1
sunshipan <sunshipan@huawei.com>
2024-04-19 02:25:31 +0800

    fix property 1
    Signed-off-by: sunshipan <sunshipan@huawei.com>

--- rosen/modules/graphics_effect/src/ge_linear_gradient_blur_shader_filter.cpp
+++ rosen/modules/graphics_effect/src/ge_linear_gradient_blur_shader_filter.cpp
@@ -109,9 +109,6 @@ std::shared_ptr<Drawing::Image> GELinearGradientBlurShaderFilter::ProcessImage(D
         return image;
     }
 
-    if (Drawing::SystemProperties::GetGpuApiType() == OHOS::Rosen::GpuApiType::DDGR) {
-        return ProcessImageDDGR(canvas, image, directionBias);
-    }
     if (GetMaskLinearBlurEnabled() && para->useMaskAlgorithm_) {
         // use faster LinearGradientBlur if valid
         if (para->LinearGradientBlurFilter_ == nullptr) {

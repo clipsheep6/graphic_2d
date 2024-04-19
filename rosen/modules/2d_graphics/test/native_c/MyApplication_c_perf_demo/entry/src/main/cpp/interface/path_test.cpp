fa16912ed7bd9db2d5c3f9297c77db2758ecd7f9
南山竹 <13588210163@139.com>
2024-04-19 19:54:27 +0800

    fix path test

--- rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/interface/path_test.cpp
+++ rosen/modules/2d_graphics/test/native_c/MyApplication_c_perf_demo/entry/src/main/cpp/interface/path_test.cpp
@@ -17,19 +17,18 @@
 void PathSetFillType::OnTestPerformance(OH_Drawing_Canvas* canvas)
 {
     TestRend rand;
-    
+    OH_Drawing_Path* path = OH_Drawing_PathCreate();
     for (int i = 0; i < testCount_; i++) {
         float l = rand.nextULessThan(bitmapWidth_);
         float t = rand.nextULessThan( bitmapHeight_);
         float r = l + rand.nextULessThan(bitmapWidth_);
         float b = t + rand.nextULessThan(bitmapHeight_);
-        OH_Drawing_Path* path = OH_Drawing_PathCreate();
         OH_Drawing_PathAddRect(path, l, t, r, b, OH_Drawing_PathDirection::PATH_DIRECTION_CCW);        
         OH_Drawing_PathSetFillType(path, fType);
         OH_Drawing_CanvasDrawPath(canvas, path);
-        OH_Drawing_PathDestroy(path);
+        OH_Drawing_PathReset(path);
     }
-    
+    OH_Drawing_PathDestroy(path);
 }
 
 void PathGetLength::OnTestPerformance(OH_Drawing_Canvas* canvas)
@@ -38,18 +37,20 @@ void PathGetLength::OnTestPerformance(OH_Drawing_Canvas* canvas)
     float length = 0;
     float x,y;
     
+    OH_Drawing_Path* path = OH_Drawing_PathCreate();
     for (int i = 0; i < testCount_; i++) {
-        OH_Drawing_Path* path = OH_Drawing_PathCreate();
-        OH_Drawing_PathMoveTo(path, 0,0);
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);            
+        OH_Drawing_PathMoveTo(path, x, y);
         x = rand.nextULessThan(bitmapWidth_);
         y = rand.nextULessThan(bitmapHeight_);        
         OH_Drawing_PathLineTo(path, x, y);
         length = OH_Drawing_PathGetLength(path, bClosed);
         OH_Drawing_CanvasDrawPath(canvas, path);
-//        DRAWING_LOGI("OnTestFunction length = %{public}f",length);
-        OH_Drawing_PathDestroy(path);
+        OH_Drawing_PathReset(path);
+        DRAWING_LOGI("OnTestFunction length = %{public}f",length);
     }
-    
+    OH_Drawing_PathDestroy(path);
 }
 
 void PathClose::OnTestPerformance(OH_Drawing_Canvas* canvas)
@@ -57,19 +58,22 @@ void PathClose::OnTestPerformance(OH_Drawing_Canvas* canvas)
     TestRend rand;
     float length = 0;
     float x,y;
-    
+    OH_Drawing_Path* path = OH_Drawing_PathCreate();
     for (int i = 0; i < testCount_; i++) {
-        OH_Drawing_Path* path = OH_Drawing_PathCreate();
-        OH_Drawing_PathMoveTo(path, 0,0);
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);            
+        OH_Drawing_PathMoveTo(path, x, y);
         x = rand.nextULessThan(bitmapWidth_);
         y = rand.nextULessThan(bitmapHeight_);        
         OH_Drawing_PathLineTo(path, x, y);
-
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);        
+        OH_Drawing_PathLineTo(path, x, y);
         OH_Drawing_PathClose(path);
         OH_Drawing_CanvasDrawPath(canvas, path);
-        OH_Drawing_PathDestroy(path);
+        OH_Drawing_PathReset(path);
     }
-    
+    OH_Drawing_PathDestroy(path);
 }
 
 void PathOffset::OnTestPerformance(OH_Drawing_Canvas* canvas)
@@ -78,36 +82,52 @@ void PathOffset::OnTestPerformance(OH_Drawing_Canvas* canvas)
     float length = 0;
     float x,y;
     OH_Drawing_Path* path = OH_Drawing_PathCreate();
+    OH_Drawing_Path* pathDst = OH_Drawing_PathCreate();
     for (int i = 0; i < testCount_; i++) {
         OH_Drawing_PathMoveTo(path, 0,0);
         x = rand.nextULessThan(bitmapWidth_);
         y = rand.nextULessThan(bitmapHeight_);        
         OH_Drawing_PathLineTo(path, x, y);
-
         x = rand.nextULessThan(bitmapWidth_);
-        y = rand.nextULessThan(bitmapHeight_);     
-        OH_Drawing_PathOffset(path, path, x, y);
-        OH_Drawing_CanvasDrawPath(canvas, path);
+        y = rand.nextULessThan(bitmapHeight_);        
+        OH_Drawing_PathLineTo(path, x, y);         
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);
+        OH_Drawing_PathOffset(path, pathDst, x, y);
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);
+        OH_Drawing_PathOffset(path, pathDst, x, y);        
+        OH_Drawing_CanvasDrawPath(canvas, pathDst);
+        OH_Drawing_PathReset(path);
+        OH_Drawing_PathReset(pathDst);
     }
+    OH_Drawing_PathDestroy(pathDst);
     OH_Drawing_PathDestroy(path);
 }
 
-void PathReset::OnTestPerformance(OH_Drawing_Canvas* canvas)
-{
+void PathReset::OnTestPerformance(OH_Drawing_Canvas *canvas) {
     TestRend rand;
     float length = 0;
     float x,y;
-    
+    OH_Drawing_Path* path = OH_Drawing_PathCreate();
     for (int i = 0; i < testCount_; i++) {
-        OH_Drawing_Path* path = OH_Drawing_PathCreate();
-        OH_Drawing_PathMoveTo(path, 0,0);
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);          
+        OH_Drawing_PathMoveTo(path, x, y);
         x = rand.nextULessThan(bitmapWidth_);
         y = rand.nextULessThan(bitmapHeight_);        
         OH_Drawing_PathLineTo(path, x, y);
-        
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);        
+        OH_Drawing_PathLineTo(path, x, y);       
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);        
+        OH_Drawing_PathLineTo(path, x, y);
+        x = rand.nextULessThan(bitmapWidth_);
+        y = rand.nextULessThan(bitmapHeight_);        
+        OH_Drawing_PathLineTo(path, x, y);          
         OH_Drawing_CanvasDrawPath(canvas, path);
         OH_Drawing_PathReset(path);
-        OH_Drawing_PathDestroy(path);
     }
-    
+    OH_Drawing_PathDestroy(path);
 }

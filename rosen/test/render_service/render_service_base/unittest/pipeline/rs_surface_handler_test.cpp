31efe474fe7594a253b0922738247dc5e8c1ab49
d00810141 <1072757164@qq.com>
2024-04-29 17:18:15 +0800

    add surface_handler tdd test
    Signed-off-by: d00810141 <1072757164@qq.com>

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_handler_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_handler_test.cpp
@@ -17,7 +17,8 @@
 
 #include "platform/common/rs_log.h"
 #include "pipeline/rs_surface_handler.h"
-
+#include "pipeline/rs_context.h"
+#include "pipeline/rs_surface_render_node.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -25,6 +26,8 @@ using namespace testing::ext;
 namespace OHOS::Rosen {
 class RSSurfaceHandlerTest : public testing::Test, public IBufferConsumerListenerClazz {
 public:
+    static inline NodeId id;
+    static inline std::weak_ptr<RSContext> context = {};
     static void SetUpTestCase() {}
     static void TearDownTestCase() {}
     void SetUp() override;
@@ -125,6 +128,117 @@ HWTEST_F(RSSurfaceHandlerTest, RegisterDeleteBufferListener001, TestSize.Level1)
     EXPECT_TRUE(rSSurfaceHandlerPtr_->preBuffer_.bufferDeleteCb_ != nullptr);
     rSSurfaceHandlerPtr_->CleanCache();
 #endif
+}
+
+/**
+ * @tc.name: ConsumeAndUpdateBuffer01
+ * @tc.desc: function test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSSurfaceHandlerTest, ConsumeAndUpdateBuffer01, TestSize.Level2)
+{
+    RSSurfaceHandler::SurfaceBufferEntry buffer;
+    auto rsSurfaceRenderNode = std::make_shared<RSSurfaceRenderNode>(id, context);
+    auto& surfaceHandler = static_cast<RSSurfaceHandler&>(*(rsSurfaceRenderNode.get()));
+
+    buffer.buffer = nullptr;
+    buffer.timestamp = 100;
+    surfaceHandler.ConsumeAndUpdateBuffer(buffer);
+    ASSERT_EQ(surfaceHandler.IsCurrentFrameBufferConsumed(), false);
+}
+
+/**
+ * @tc.name: CacheBuffer01
+ * @tc.desc: function test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSSurfaceHandlerTest, CacheBuffer01, TestSize.Level2)
+{
+    RSSurfaceHandler::SurfaceBufferEntry buffer;
+    auto rsSurfaceRenderNode = std::make_shared<RSSurfaceRenderNode>(id, context);
+    auto& surfaceHandler = static_cast<RSSurfaceHandler&>(*(rsSurfaceRenderNode.get()));
+
+    buffer.buffer = nullptr;
+    buffer.timestamp = 100;
+    surfaceHandler.CacheBuffer(buffer);
+    ASSERT_EQ(surfaceHandler.bufferCache_.size(), 1);
+}
+
+/**
+ * @tc.name: GetBufferFromCache01
+ * @tc.desc: function test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSSurfaceHandlerTest, GetBufferFromCache01, TestSize.Level2)
+{
+    RSSurfaceHandler::SurfaceBufferEntry buffer1;
+    RSSurfaceHandler::SurfaceBufferEntry buffer2;
+    auto rsSurfaceRenderNode = std::make_shared<RSSurfaceRenderNode>(id, context);
+    auto& surfaceHandler = static_cast<RSSurfaceHandler&>(*(rsSurfaceRenderNode.get()));
+
+    buffer1.buffer = nullptr;
+    buffer1.timestamp = 100;
+    surfaceHandler.CacheBuffer(buffer1);
+
+    buffer2.buffer = nullptr;
+    buffer2.timestamp = 200;
+    surfaceHandler.CacheBuffer(buffer2);
+
+    uint64_t vsyncTimestamp = 50;
+    ASSERT_EQ(surfaceHandler.GetBufferFromCache(vsyncTimestamp).timestamp, 0);
+}
+
+/**
+ * @tc.name: GetBufferFromCache02
+ * @tc.desc: function test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSSurfaceHandlerTest, GetBufferFromCache02, TestSize.Level2)
+{
+    RSSurfaceHandler::SurfaceBufferEntry buffer1;
+    RSSurfaceHandler::SurfaceBufferEntry buffer2;
+    auto rsSurfaceRenderNode = std::make_shared<RSSurfaceRenderNode>(id, context);
+    auto& surfaceHandler = static_cast<RSSurfaceHandler&>(*(rsSurfaceRenderNode.get()));
+
+    buffer1.buffer = nullptr;
+    buffer1.timestamp = 100;
+    surfaceHandler.CacheBuffer(buffer1);
+
+    buffer2.buffer = nullptr;
+    buffer2.timestamp = 200;
+    surfaceHandler.CacheBuffer(buffer2);
+
+    uint64_t vsyncTimestamp = 150;
+    ASSERT_EQ(surfaceHandler.GetBufferFromCache(vsyncTimestamp).timestamp, 100);
+}
+
+/**
+ * @tc.name: GetBufferFromCache03
+ * @tc.desc: function test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSSurfaceHandlerTest, GetBufferFromCache03, TestSize.Level2)
+{
+    RSSurfaceHandler::SurfaceBufferEntry buffer1;
+    RSSurfaceHandler::SurfaceBufferEntry buffer2;
+    auto rsSurfaceRenderNode = std::make_shared<RSSurfaceRenderNode>(id, context);
+    auto& surfaceHandler = static_cast<RSSurfaceHandler&>(*(rsSurfaceRenderNode.get()));
+
+    buffer1.buffer = nullptr;
+    buffer1.timestamp = 100;
+    surfaceHandler.CacheBuffer(buffer1);
+
+    buffer2.buffer = nullptr;
+    buffer2.timestamp = 200;
+    surfaceHandler.CacheBuffer(buffer2);
+
+    uint64_t vsyncTimestamp = 300;
+    ASSERT_EQ(surfaceHandler.GetBufferFromCache(vsyncTimestamp).timestamp, 200);
 }
 
 } // namespace OHOS::Rosen

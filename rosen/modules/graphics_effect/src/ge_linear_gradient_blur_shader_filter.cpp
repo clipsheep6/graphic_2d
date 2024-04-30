adf38764cfcd7780ff7bf89a12f967adf8d91a6c
qxk <14165546+qinxikui@user.noreply.gitee.com>
2024-04-30 07:42:07 +0000

    update rosen/modules/graphics_effect/src/ge_linear_gradient_blur_shader_filter.cpp.
    Signed-off-by: qxk <14165546+qinxikui@user.noreply.gitee.com>

--- rosen/modules/graphics_effect/src/ge_linear_gradient_blur_shader_filter.cpp
+++ rosen/modules/graphics_effect/src/ge_linear_gradient_blur_shader_filter.cpp
@@ -284,7 +284,7 @@ std::shared_ptr<Drawing::ShaderEffect> GELinearGradientBlurShaderFilter::MakeAlp
         p.emplace_back(para->fractionStops_[i].second);
     }
     
-    if (para->fractionStops_.size() < 1) {
+    if (para->fractionStops_.empty()) {
         return nullptr;
     }
     if (para->fractionStops_.back().second < 0.99) { // 0.99 represents the fraction bias

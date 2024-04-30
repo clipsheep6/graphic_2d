c8be35bde9bbcde1c10ca13c8eb6b682d5ad1b40
yangfan479 <yangfan479@huawei.com>
2024-04-30 12:09:36 +0800

    [拆流水适配]多屏下主屏使能脏区
    Signed-off-by: yangfan479 <yangfan479@huawei.com>

--- rosen/modules/render_service_base/include/params/rs_render_thread_params.h
+++ rosen/modules/render_service_base/include/params/rs_render_thread_params.h
@@ -227,6 +227,16 @@ public:
     {
         return context_.lock();
     }
+
+    void SetClipRegion(const Drawing::Region& clipRegion)
+    {
+        clipRegion_.Clone(clipRegion);
+    }
+
+    const Drawing::Region& GetClipRegion() const
+    {
+        return clipRegion_;
+    }
 private:
     bool startVisit_ = false;
     bool hasCaptureImg_ = false;
@@ -261,6 +271,8 @@ private:
     int64_t onVsyncStartTimeSteady_ = TIMESTAMP_INITIAL;
     bool isUniRenderAndOnVsync_ = false;
     std::weak_ptr<RSContext> context_;
+
+    Drawing::Region clipRegion_;
 
     friend class RSMainThread;
     friend class RSUniRenderVisitor;

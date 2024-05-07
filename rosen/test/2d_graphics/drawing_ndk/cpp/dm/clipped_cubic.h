537fdde49f0a18eeb9b9ffb3c2705c2e90bbec32
秦莉文 <qinliwen3@huawei.com>
2024-05-07 21:05:11 +0800

    drawing ndk dm format issue Signed-off-by: qinliwen <qinliwen3@huawei.com>

--- rosen/test/2d_graphics/drawing_ndk/cpp/dm/clipped_cubic.h
+++ rosen/test/2d_graphics/drawing_ndk/cpp/dm/clipped_cubic.h
@@ -14,17 +14,17 @@
  */
 #ifndef CLIPPED_CUBIC_H
 #define CLIPPED_CUBIC_H
+#include <native_drawing/drawing_canvas.h>
+#include <native_drawing/drawing_rect.h>
+
 #include "../test_base.h"
 
-#include <native_drawing/drawing_rect.h>
-#include <native_drawing/drawing_canvas.h>
-
 class ClippedCubic : public TestBase {
-  public:
+public:
     ClippedCubic();
     ~ClippedCubic() override {};
 
-  protected:
-    void OnTestFunction(OH_Drawing_Canvas *canvas) override;
+protected:
+    void OnTestFunction(OH_Drawing_Canvas* canvas) override;
 };
 #endif // CLIPPED_CUBIC_H

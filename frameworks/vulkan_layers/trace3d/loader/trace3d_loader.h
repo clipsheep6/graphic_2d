4fe51756018df7b366e9b7710997c6fdfb244292
Ivan Nazarov <nazarov.ivan@huawei-partners.com>
2024-05-08 06:57:07 -0700

    Add Trace3D Loader
    Signed-off-by: Ivan Nazarov <nazarov.ivan@huawei-partners.com>

---
+++ frameworks/vulkan_layers/trace3d/loader/trace3d_loader.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
+//
+
+//
+// Desc: This file is a part of the Trace3D Capture Gles/Vk Layer Loader
+//
+// Author: Dmitry Zhuk Z00858759 <zhuk.dmitry@huawei.com>
+//
+
+namespace trace3d {
+
+void* CaptureInit();
+void CaptureCleanup();
+
+} // namespace trace3d
+

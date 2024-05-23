7053c69d9f98f20a1e8c86d4452d413062c3b860
wangwei <changleipeng4@huawei.com>
2024-05-23 11:05:05 +0800

    ww
    Signed-off-by: wangwei <changleipeng4@huawei.com>

--- rosen/test/render_service/render_service_base/fuzztest/rsscreenhdrcapability_fuzzer/rsscreenhdrcapability_fuzzer.h
+++ rosen/test/render_service/render_service_base/fuzztest/rsscreenhdrcapability_fuzzer/rsscreenhdrcapability_fuzzer.h
@@ -13,6 +13,9 @@
  * limitations under the License.
  */
 
+#ifndef RSSCREENHDRCAPABILITY_FUZZER_H
+#define RSSCREENHDRCAPABILITY_FUZZER_H
+
 #include <cstdint>
 #include <unistd.h>
 #include <climits>
@@ -22,3 +25,4 @@
 
 #define FUZZ_PROJECT_NAME "rsscreenhdrcapability_fuzzer"
 
+#endif // RSSCREENHDRCAPABILITY_FUZZER_H

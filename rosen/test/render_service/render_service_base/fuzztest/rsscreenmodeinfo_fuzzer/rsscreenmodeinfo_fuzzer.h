42698c37530ca2a826d6b869a2ef5598e8c7a296
wangwei <changleipeng4@huawei.com>
2024-05-23 14:25:07 +0800

    ww
    Signed-off-by: wangwei <changleipeng4@huawei.com>

--- rosen/test/render_service/render_service_base/fuzztest/rsscreenmodeinfo_fuzzer/rsscreenmodeinfo_fuzzer.h
+++ rosen/test/render_service/render_service_base/fuzztest/rsscreenmodeinfo_fuzzer/rsscreenmodeinfo_fuzzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +13,9 @@
  * limitations under the License.
  */
 
+#ifndef RSSCREENMODEINFO_FUZZER_H
+#define RSSCREENMODEINFO_FUZZER_H
+
 #include <cstdint>
 #include <unistd.h>
 #include <climits>
@@ -22,3 +25,4 @@
 
 #define FUZZ_PROJECT_NAME "rsscreenmodeinfo_fuzzer"
 
+#endif // RSSCREENMODEINFO_FUZZER_H

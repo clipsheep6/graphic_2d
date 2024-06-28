f9e72e8746d1c32d3bb3ea42005ccc48b1dab947
dongzhengkuan <dongzhengkuan@huawei.com>
2024-06-28 07:27:44 +0000

    add pointer color inversion ipc interface
    Signed-off-by: dongzhengkuan <dongzhengkuan@huawei.com>

--- rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp
+++ rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp
@@ -111,21 +111,33 @@ void RSInterfaces::RemoveVirtualScreen(ScreenId id)
 
 int32_t RSInterfaces::SetPointerColorInversionConfig(float darkBuffer, float brightBuffer, int64_t interval)
 {
+    if (renderServiceClient_ == nullptr) {
+        return StatusCode::RENDER_SERVICE_NULL;
+    }
     return renderServiceClient_->SetPointerColorInversionConfig(darkBuffer, brightBuffer, interval);
 }
  
 int32_t RSInterfaces::SetPointerColorInversionEnabled(bool enable)
 {
+    if (renderServiceClient_ == nullptr) {
+        return StatusCode::RENDER_SERVICE_NULL;
+    }
     return renderServiceClient_->SetPointerColorInversionEnabled(enable);
 }
  
 int32_t RSInterfaces::RegisterPointerLuminanceChangeCallback(const PointerLuminanceChangeCallback &callback)
 {
+    if (renderServiceClient_ == nullptr) {
+        return StatusCode::RENDER_SERVICE_NULL;
+    }
     return renderServiceClient_->RegisterPointerLuminanceChangeCallback(callback);
 }
  
 int32_t RSInterfaces::UnRegisterPointerLuminanceChangeCallback()
 {
+    if (renderServiceClient_ == nullptr) {
+        return StatusCode::RENDER_SERVICE_NULL;
+    }
     return renderServiceClient_->UnRegisterPointerLuminanceChangeCallback();
 }
 

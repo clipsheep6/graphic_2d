4695fa678351b3ada313e1b313b416e4a4e354d3
LeesonWong <leesonwong_mouse@outlook.com>
2024-06-29 09:40:53 +0800

    Test cases for rs_pixel_map_util
    Signed-off-by: w30027672 <wanglesheng@huawei.com>

---
+++ rosen/modules/render_service_base/test/unittest/render/rs_pixel_map_util_test.cpp
@@ -0,0 +1,95 @@
+/*
+* Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "render/rs_pixel_map_util.h"
+#include "pixel_map.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+class RsPixelMapUtilTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void RsPixelMapUtilTest::SetUpTestCase() {}
+void RsPixelMapUtilTest::TearDownTestCase() {}
+
+void RsPixelMapUtilTest::SetUp() {}
+void RsPixelMapUtilTest::TearDown() {}
+
+HWTEST_F(RsPixelMapUtilTest, ExtractDrawingImageTest001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest ExtractDrawingImageTest001 start";
+
+    ExtractDrawingImage::ExtractDrawingImageTest();
+
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest ExtractDrawingImageTest001 end";
+}
+
+HWTEST_F(RsPixelMapUtilTest, TransformDataSetForAstc001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest TransformDataSetForAstc001 start";
+
+    ExtractDrawingImage::TransformDataSetForAstc();
+
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest TransformDataSetForAstc001 end";
+}
+
+HWTEST_F(RsPixelMapUtilTest, DrawPixelMap001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest DrawPixelMap001 start";
+
+    ExtractDrawingImage::DrawPixelMap();
+
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest DrawPixelMap001 end";
+}
+
+HWTEST_F(RsPixelMapUtilTest, IsYUVFormat001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest IsYUVFormat001 start";
+
+    ExtractDrawingImage::IsYUVFormat();
+
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest IsYUVFormat001 end";
+}
+
+HWTEST_F(RsPixelMapUtilTest, IsSupportZeroCopy001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest IsSupportZeroCopy001 start";
+
+    ExtractDrawingImage::IsSupportZeroCopy();
+
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest IsSupportZeroCopy001 end";
+}
+
+HWTEST_F(RsPixelMapUtilTest, ConvertYUVPixelMapToDrawingImage001, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest ConvertYUVPixelMapToDrawingImage001 start";
+
+    ExtractDrawingImage::ConvertYUVPixelMapToDrawingImage();
+
+    GTEST_LOG_(INFO) << "RSAnimationFractionTest ConvertYUVPixelMapToDrawingImage001 end";
+}
+} // Rosen
+} // OHOS
+

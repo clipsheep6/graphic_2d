d01054bec42d56f558525de8aeef09ecb25b433c
liuwei <liuwei793@h-partners.com>
2024-07-05 12:02:10 +0800

    begin和finish函数实现
    Signed-off-by: liuwei <liuwei793@h-partners.com>
    Change-Id: If052165d4ed625f356afaf6d707775b01b409d06

--- rosen/modules/2d_graphics/include/image/picture_recorder.h
+++ rosen/modules/2d_graphics/include/image/picture_recorder.h
@@ -13,24 +13,26 @@
  * limitations under the License.
  */
 
+#ifndef PICTURE_RECORDER_H
+#define PICTURE_RECORDER_H
+
+#include "recording/recording_canvas.h"
+#include "recording/draw_cmd_list.h"
 #include "image/picture.h"
 
 namespace OHOS {
 namespace Rosen {
 namespace Drawing {
-Picture::Picture(std::shared_ptr<DrawCmdList> cmdList)
-    : drawCmdList(cmdList) {
-}
-
-Picture::~Picture() {
-}
-
-void Picture::Playback(Canvas* canvas) {
-    if (drawCmdList) {
-        drawCmdList->Playback(*canvas);
-    }
-}
-
+class PictureRecorder {
+public:
+    PictureRecorder();
+    ~PictureRecorder();
+    std::shared_ptr<RecordingCanvas> BeginRecording(int32_t width, int32_t height);
+    std::shared_ptr<Picture> FinishingRecording();
+private:
+    std::shared_ptr<RecordingCanvas> record = nullptr;
+};
 } // namespace Drawing
 } // namespace Rosen
 } // namespace OHOS
+#endif

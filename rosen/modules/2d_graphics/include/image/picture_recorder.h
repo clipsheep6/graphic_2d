4d96d2d5fe9163ac3c2800e0ddeba95240ef4be8
liuwei <liuwei793@h-partners.com>
2024-07-09 14:57:13 +0800

    CoreCanvas的drawpicture
    Signed-off-by: liuwei <liuwei793@h-partners.com>
    Change-Id: I355517d6c22b441340004d8d328b2e632620f0f3

---
+++ rosen/modules/2d_graphics/include/image/picture_recorder.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PICTURE_RECORDER_H
+#define PICTURE_RECORDER_H
+
+#include "recording/recording_canvas.h"
+#include "recording/draw_cmd_list.h"
+#include "image/picture.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace Drawing {
+class PictureRecorder {
+public:
+    PictureRecorder();
+    ~PictureRecorder();
+    std::shared_ptr<RecordingCanvas> BeginRecording(int32_t width, int32_t height);
+    std::shared_ptr<Picture> FinishingRecording();
+private:
+    std::shared_ptr<RecordingCanvas> record = nullptr;
+};
+} // namespace Drawing
+} // namespace Rosen
+} // namespace OHOS
+#endif

fdd045d76f472132841f52941d97dafcc63fd2fe
钱常德 <qianchangde@huawei.com>
2024-07-12 20:32:00 +0800

    Node rectification in uniprocess
    Signed-off-by: 钱常德 <qianchangde@huawei.com>
    Change-Id: Ia74f856ef2520e8b8e21deacafaeaab66f16ebd6

--- rosen/modules/render_service_base/include/params/rs_display_render_params.h
+++ rosen/modules/render_service_base/include/params/rs_display_render_params.h
@@ -101,6 +101,10 @@ public:
     {
         return hardwareEnabledTopNodes_;
     }
+    bool GetSecurityDisplay() const
+    {
+        return isSecurityDisplay_;
+    }
     void SetMainAndLeashSurfaceDirty(bool isDirty);
     bool GetMainAndLeashSurfaceDirty() const;
     bool HasSecurityLayer();
@@ -124,6 +128,10 @@ public:
     // dfx
     std::string ToString() const override;
 
+    std::shared_ptr<DrawableV2::RSRenderNodeDrawableAdapter> GetMirrorSourceDrawable()
+    {
+        return mirrorSourceDrawable_;
+    }
 private:
     std::map<ScreenId, bool> displayHasSecSurface_;
     std::map<ScreenId, bool> displayHasSkipSurface_;
@@ -136,7 +144,9 @@ private:
     ScreenRotation nodeRotation_ = ScreenRotation::INVALID_SCREEN_ROTATION;
     ScreenRotation screenRotation_ = ScreenRotation::INVALID_SCREEN_ROTATION;
     uint64_t screenId_ = 0;
+    bool isSecurityDisplay_ = false;
     std::weak_ptr<RSDisplayRenderNode> mirrorSource_;
+    std::shared_ptr<DrawableV2::RSRenderNodeDrawableAdapter> mirrorSourceDrawable_ = nullptr;
     NodeId mirrorSourceId_ = INVALID_NODEID;
     ScreenInfo screenInfo_;
     ScreenId mirroredId_ = INVALID_SCREEN_ID;
